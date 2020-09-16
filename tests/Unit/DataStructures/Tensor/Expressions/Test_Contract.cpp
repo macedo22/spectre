46e91d34d5d9ea6d910934482e927ac22f007892
Alexandra Macedo <amacedo@csu.fullerton.edu>
2020-09-15 22:03:08 -0700

    Add broken contract test

---
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_Contract.cpp
@@ -0,0 +1,32 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cstddef>
+#include <iterator>
+#include <numeric>
+
+#include "DataStructures/Tensor/Expressions/Contract.hpp"
+#include "DataStructures/Tensor/Expressions/Evaluate.hpp"
+#include "DataStructures/Tensor/Expressions/TensorExpression.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+
+SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.Contract",
+                  "[DataStructures][Unit]") {
+  Tensor<double, Symmetry<2, 1>,
+         index_list<SpacetimeIndex<3, UpLo::Up, Frame::Grid>,
+                    SpacetimeIndex<3, UpLo::Lo, Frame::Grid>>>
+      Aul{};
+  std::iota(Aul.begin(), Aul.end(), 0.0);
+
+  auto tensor = TensorExpressions::evaluate(Aul(ti_A, ti_a));
+  auto result = tensor.get();
+
+  double sum = 0.0;
+  for (int i = 0; i < 3; ++i) {
+    sum += Aul.get(i, i);
+  }
+
+  CHECK(sum == result);
+}
