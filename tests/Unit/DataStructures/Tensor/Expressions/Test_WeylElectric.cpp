5e0f3e01f9949e0ab175a079076316373d15294c
Alexandra Macedo <amacedo@csu.fullerton.edu>
2020-12-12 03:12:04 -0800

    Assert LHS tensor type of weyl electric test

--- tests/Unit/DataStructures/Tensor/Expressions/Test_WeylElectric.cpp
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_WeylElectric.cpp
@@ -78,12 +78,17 @@ SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.TEWeylElectric",
   auto og_weyl_electric_tensor = weyl_electric<3, Frame::Inertial, double>(
       spatial_ricci, extrinsic_curvature, inverse_spatial_metric);
 
-  auto TE_weyl_electric_tensor = TensorExpressions::evaluate<ti_i_t, ti_j_t>(
-      (spatial_ricci(ti_i, ti_j)) +
-      ((extrinsic_curvature(ti_k, ti_l) * inverse_spatial_metric(ti_K, ti_L)) *
-       extrinsic_curvature(ti_i, ti_j)) -
-      ((extrinsic_curvature(ti_i, ti_l) * inverse_spatial_metric(ti_K, ti_L)) *
-       extrinsic_curvature(ti_k, ti_j)));
+  Tensor<double, Symmetry<2, 1>,
+         index_list<SpatialIndex<3, UpLo::Lo, Frame::Inertial>,
+                    SpatialIndex<3, UpLo::Lo, Frame::Inertial>>>
+      TE_weyl_electric_tensor = TensorExpressions::evaluate<ti_i_t, ti_j_t>(
+          (spatial_ricci(ti_i, ti_j)) +
+          ((extrinsic_curvature(ti_k, ti_l) *
+            inverse_spatial_metric(ti_K, ti_L)) *
+           extrinsic_curvature(ti_i, ti_j)) -
+          ((extrinsic_curvature(ti_i, ti_l) *
+            inverse_spatial_metric(ti_K, ti_L)) *
+           extrinsic_curvature(ti_k, ti_j)));
 
   for (size_t i = 0; i < 3; i++) {
     for (size_t j = 0; j < 3; j++) {
