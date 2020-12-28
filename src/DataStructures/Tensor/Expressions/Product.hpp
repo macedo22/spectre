fc40917d8ee162d19de43f2577dec73e97966fe6
Alexandra Macedo <amacedo@csu.fullerton.edu>
2020-12-27 20:08:29 -0800

    Rename Product->OuterProduct

--- src/DataStructures/Tensor/Expressions/Product.hpp
+++ src/DataStructures/Tensor/Expressions/Product.hpp
@@ -2,7 +2,7 @@
 // See LICENSE.txt for details.
 
 /// \file
-/// Defines ET for tensor products
+/// Defines ET for tensor inner and outer products
 
 #pragma once
 
@@ -20,12 +20,12 @@ namespace TensorExpressions {
 namespace detail {
 template <typename T1, typename T2, typename SymmList1 = typename T1::symmetry,
           typename SymmList2 = typename T2::symmetry>
-struct ProductType;
+struct OuterProductType;
 
 template <typename T1, typename T2, template <typename...> class SymmList1,
           typename... Symm1, template <typename...> class SymmList2,
           typename... Symm2>
-struct ProductType<T1, T2, SymmList1<Symm1...>, SymmList2<Symm2...>> {
+struct OuterProductType<T1, T2, SymmList1<Symm1...>, SymmList2<Symm2...>> {
   using symmetry =
       Symmetry<(Symm1::value + sizeof...(Symm2))..., Symm2::value...>;
   using index_list =
@@ -44,30 +44,31 @@ struct ProductType<T1, T2, SymmList1<Symm1...>, SymmList2<Symm2...>> {
  * @tparam ArgsList2 eh
  */
 template <typename T1, typename T2, typename ArgsList1, typename ArgsList2>
-struct Product;
+struct OuterProduct;
 
 template <typename T1, typename T2, template <typename...> class ArgsList1,
           template <typename...> class ArgsList2, typename... Args1,
           typename... Args2>
-struct Product<T1, T2, ArgsList1<Args1...>, ArgsList2<Args2...>>
+struct OuterProduct<T1, T2, ArgsList1<Args1...>, ArgsList2<Args2...>>
     : public TensorExpression<
-          Product<T1, T2, ArgsList1<Args1...>, ArgsList2<Args2...>>,
-          typename T1::type, typename detail::ProductType<T1, T2>::symmetry,
-          typename detail::ProductType<T1, T2>::index_list,
-          typename detail::ProductType<T1, T2>::tensorindex_list> {
+          OuterProduct<T1, T2, ArgsList1<Args1...>, ArgsList2<Args2...>>,
+          typename T1::type,
+          typename detail::OuterProductType<T1, T2>::symmetry,
+          typename detail::OuterProductType<T1, T2>::index_list,
+          typename detail::OuterProductType<T1, T2>::tensorindex_list> {
   static_assert(std::is_same<typename T1::type, typename T2::type>::value,
                 "Cannot product Tensors holding different data types.");
   using type = typename T1::type;
-  using symmetry = typename detail::ProductType<T1, T2>::symmetry;
-  using index_list = typename detail::ProductType<T1, T2>::index_list;
-  using args_list = typename detail::ProductType<T1, T2>::tensorindex_list;
+  using symmetry = typename detail::OuterProductType<T1, T2>::symmetry;
+  using index_list = typename detail::OuterProductType<T1, T2>::index_list;
+  using args_list = typename detail::OuterProductType<T1, T2>::tensorindex_list;
   static constexpr auto num_tensor_indices = tmpl::size<index_list>::value;
   static constexpr auto num_tensor_indices_first_operand =
       tmpl::size<typename T1::index_list>::value;
   static constexpr auto num_tensor_indices_second_operand =
       num_tensor_indices - num_tensor_indices_first_operand;
 
-  Product(const T1& t1, const T2& t2)
+  OuterProduct(const T1& t1, const T2& t2)
       : t1_(std::move(t1)), t2_(std::move(t2)) {}
 
   /// \brief Get a product expression operand's generic indices in the order
@@ -130,12 +131,12 @@ struct Product<T1, T2, ArgsList1<Args1...>, ArgsList2<Args2...>>
 
   template <typename FirstOpLhsTensorIndexList,
             typename SecondOpLhsTensorIndexList>
-  struct ComputeProduct;
+  struct ComputeOuterProduct;
 
   template <typename... FirstOpLhsTensorIndices,
             typename... SecondOpLhsTensorIndices>
-  struct ComputeProduct<tmpl::list<FirstOpLhsTensorIndices...>,
-                        tmpl::list<SecondOpLhsTensorIndices...>> {
+  struct ComputeOuterProduct<tmpl::list<FirstOpLhsTensorIndices...>,
+                             tmpl::list<SecondOpLhsTensorIndices...>> {
     template <typename FirstOpUncontractedLhsStructure,
               typename SecondOpUncontractedLhsStructure>
     static SPECTRE_ALWAYS_INLINE decltype(auto) apply(
@@ -187,8 +188,8 @@ struct Product<T1, T2, ArgsList1<Args1...>, ArgsList2<Args2...>>
         second_op_uncontracted_lhs_structure::get_storage_index(
             second_op_lhs_tensor_multi_index);
 
-    return ComputeProduct<first_op_lhs_tensorindex_list,
-                          second_op_lhs_tensorindex_list>::
+    return ComputeOuterProduct<first_op_lhs_tensorindex_list,
+                               second_op_lhs_tensorindex_list>::
         template apply<first_op_uncontracted_lhs_structure,
                        second_op_uncontracted_lhs_structure>(
             first_storage_index_operand, second_storage_index_operand, t1_,
@@ -225,7 +226,7 @@ SPECTRE_ALWAYS_INLINE auto operator*(
     const TensorExpression<T1, X, Symm1, IndexList1, Args1>& t1,
     const TensorExpression<T2, X, Symm2, IndexList2, Args2>& t2) {
   return TensorExpressions::contract(
-      TensorExpressions::Product<
+      TensorExpressions::OuterProduct<
           typename std::conditional<
               std::is_base_of<Expression, T1>::value, T1,
               TensorExpression<T1, X, Symm1, IndexList1, Args1>>::type,
