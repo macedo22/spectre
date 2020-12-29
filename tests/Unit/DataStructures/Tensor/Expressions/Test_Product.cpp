5ceb696afe0e8bb7188f8a8cd2194027965acc3a
Alexandra Macedo <amacedo@csu.fullerton.edu>
2020-12-28 22:15:18 -0800

    Remove old rank 0 rank 1 outer product test

--- tests/Unit/DataStructures/Tensor/Expressions/Test_Product.cpp
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_Product.cpp
@@ -73,20 +73,6 @@ SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.Product",
   test_products(DataVector(5, std::numeric_limits<double>::signaling_NaN()));
 }
 
-SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.OuterProduct0By1",
-                  "[DataStructures][Unit]") {
-  const Tensor<double> R{{{3.7}}};
-  Tensor<double, Symmetry<1>,
-         index_list<SpacetimeIndex<3, UpLo::Up, Frame::Grid>>>
-      Su{};
-  std::iota(Su.begin(), Su.end(), 0.0);
-  auto L_A = TensorExpressions::evaluate<ti_A>(R() * Su(ti_A));
-
-  for (size_t a = 0; a < 4; a++) {
-    CHECK(L_A.get(a) == 3.7 * Su.get(a));
-  }
-}
-
 SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.OuterProduct0By2",
                   "[DataStructures][Unit]") {
   const Tensor<double> R{{{4.2}}};
