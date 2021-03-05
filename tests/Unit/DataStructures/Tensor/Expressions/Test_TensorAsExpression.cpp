bbee2f2a91509e91ce1ac351573609bfdbae3dba
Alexandra Macedo <amacedo@csu.fullerton.edu>
2021-03-04 19:24:52 -0800

    Add rank 1 TensorAsExpression test

--- tests/Unit/DataStructures/Tensor/Expressions/Test_TensorAsExpression.cpp
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_TensorAsExpression.cpp
@@ -4,12 +4,16 @@
 #include "Framework/TestingFramework.hpp"
 
 #include "DataStructures/Tensor/Expressions/TensorExpression.hpp"
+#include "Helpers/DataStructures/Tensor/Expressions/TensorAsExpressionRank1TestHelpers.hpp"
 #include "Helpers/DataStructures/Tensor/Expressions/TensorAsExpressionRank2TestHelpers.hpp"
 #include "Helpers/DataStructures/Tensor/Expressions/TensorAsExpressionRank3TestHelpers.hpp"
 #include "Helpers/DataStructures/Tensor/Expressions/TensorAsExpressionRank4TestHelpers.hpp"
 
 SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.TensorAsExpression",
                   "[DataStructures][Unit]") {
+  // Rank 1
+  TestHelpers::TensorExpressions::test_tensor_as_expression_rank_1(ti_k);
+
   // Rank 2
   TestHelpers::TensorExpressions::test_tensor_as_expression_rank_2(ti_i, ti_j);
   TestHelpers::TensorExpressions::test_tensor_as_expression_rank_2(ti_j, ti_i);
