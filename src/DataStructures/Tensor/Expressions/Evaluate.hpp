8b0cf6ac02af3cba73ea0b171011083c832f7d2f
Alexandra Macedo <amacedo@csu.fullerton.edu>
2021-03-08 20:00:53 -0800

    fixup! Add TensorExpressions::evaluate overload that takes LHS Tensor as arg

--- src/DataStructures/Tensor/Expressions/Evaluate.hpp
+++ src/DataStructures/Tensor/Expressions/Evaluate.hpp
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <type_traits>
+
 #include "DataStructures/Tensor/Expressions/LhsTensorSymmAndIndices.hpp"
 #include "DataStructures/Tensor/Expressions/TensorExpression.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
@@ -41,11 +43,11 @@ constexpr bool contains_indices_to_contract(
  * set in the template parameters
  *
  * \details Uses the right hand side (RHS) TensorExpression's index ordering
- * (`T::args_list`) and the desired left hand side (LHS) tensor's index ordering
- * (`LhsTensorIndices`) to construct a LHS Tensor with that LHS index ordering.
- * This can carry out the evaluation of a RHS tensor expression to a LHS tensor
- * with the same index ordering, such as \f$L_{ab} = R_{ab}\f$, or different
- * ordering, such as \f$L_{ba} = R_{ab}\f$.
+ * (`RhsTE::args_list`) and the desired left hand side (LHS) tensor's index
+ * ordering (`LhsTensorIndices`) to construct a LHS Tensor with that LHS index
+ * ordering. This can carry out the evaluation of a RHS tensor expression to a
+ * LHS tensor with the same index ordering, such as \f$L_{ab} = R_{ab}\f$, or
+ * different ordering, such as \f$L_{ba} = R_{ab}\f$.
  *
  * ### Example usage
  * Given two rank 2 Tensors `R` and `S` with index order (a, b), add them
@@ -63,16 +65,16 @@ constexpr bool contains_indices_to_contract(
  *
  * @tparam LhsTensorIndices the TensorIndexs of the Tensor on the LHS of the
  * tensor expression, e.g. `ti_a`, `ti_b`, `ti_c`
- * @param rhs_te the RHS TensorExpression to be evaluated
+ * @param rhs_tensorexpression the RHS TensorExpression to be evaluated
  * @return the resultant LHS Tensor with index order specified by
  * LhsTensorIndices
  */
-template <auto&... LhsTensorIndices, typename TE,
-          Requires<std::is_base_of<Expression, TE>::value> = nullptr>
-auto evaluate(const TE& rhs_te) {
+template <auto&... LhsTensorIndices, typename RhsTE,
+          Requires<std::is_base_of_v<Expression, RhsTE>> = nullptr>
+auto evaluate(const RhsTE& rhs_tensorexpression) {
   using lhs_tensorindex_list =
       tmpl::list<std::decay_t<decltype(LhsTensorIndices)>...>;
-  using rhs_tensorindex_list = typename TE::args_list;
+  using rhs_tensorindex_list = typename RhsTE::args_list;
   static_assert(
       tmpl::equal_members<lhs_tensorindex_list, rhs_tensorindex_list>::value,
       "The generic indices on the LHS of a tensor equation (that is, the "
@@ -90,8 +92,8 @@ auto evaluate(const TE& rhs_te) {
           {{std::decay_t<decltype(LhsTensorIndices)>::value...}}),
       "Cannot evaluate a tensor expression to a LHS tensor with generic "
       "indices that would be contracted, e.g. evaluate<ti_A, ti_a>.");
-  using rhs_symmetry = typename TE::symmetry;
-  using rhs_tensorindextype_list = typename TE::index_list;
+  using rhs_symmetry = typename RhsTE::symmetry;
+  using rhs_tensorindextype_list = typename RhsTE::index_list;
 
   // Stores (potentially reordered) symmetry and indices needed for constructing
   // the LHS tensor, with index order specified by LhsTensorIndices
@@ -100,10 +102,10 @@ auto evaluate(const TE& rhs_te) {
                               rhs_symmetry, rhs_tensorindextype_list>;
 
   // Construct and return LHS tensor
-  return Tensor<typename TE::type,
+  return Tensor<typename RhsTE::type,
                 typename lhs_tensor_symm_and_indices::symmetry,
                 typename lhs_tensor_symm_and_indices::tensorindextype_list>(
-      rhs_te, lhs_tensorindex_list{});
+      rhs_tensorexpression, lhs_tensorindex_list{});
 }
 
 /*!
@@ -112,11 +114,11 @@ auto evaluate(const TE& rhs_te) {
  * set in the template parameters
  *
  * \details Uses the right hand side (RHS) TensorExpression's index ordering
- * (`T::args_list`) and the desired left hand side (LHS) tensor's index ordering
- * (`LhsTensorIndices`) to fill the provided LHS Tensor with that LHS index
- * ordering. This can carry out the evaluation of a RHS tensor expression to a
- * LHS tensor with the same index ordering, such as \f$L_{ab} = R_{ab}\f$, or
- * different ordering, such as \f$L_{ba} = R_{ab}\f$.
+ * (`RhsTE::args_list`) and the desired left hand side (LHS) tensor's index
+ * ordering (`LhsTensorIndices`) to fill the provided LHS Tensor with that LHS
+ * index ordering. This can carry out the evaluation of a RHS tensor expression
+ * to a LHS tensor with the same index ordering, such as \f$L_{ab} = R_{ab}\f$,
+ * or different ordering, such as \f$L_{ba} = R_{ab}\f$.
  *
  * ### Example usage
  * Given two rank 2 Tensors `R` and `S` with index order (a, b), add them
@@ -135,15 +137,17 @@ auto evaluate(const TE& rhs_te) {
  * @tparam LhsTensorIndices the TensorIndexs of the Tensor on the LHS of the
  * tensor expression, e.g. `ti_a`, `ti_b`, `ti_c`
  * @param lhs_tensor pointer to the resultant LHS Tensor to fill
- * @param rhs_te the RHS TensorExpression to be evaluated
+ * @param rhs_tensorexpression the RHS TensorExpression to be evaluated
  */
-template <auto&... LhsTensorIndices, typename T, typename TE,
-          Requires<Tensor_detail::is_tensor<T>::value> = nullptr,
-          Requires<std::is_base_of<Expression, TE>::value> = nullptr>
-void evaluate(const gsl::not_null<T*> lhs_tensor, const TE& rhs_te) {
+template <auto&... LhsTensorIndices, typename X, typename LhsSymmetry,
+          typename LhsIndexList, typename RhsTE,
+          Requires<std::is_base_of_v<Expression, RhsTE>> = nullptr>
+void evaluate(
+    const gsl::not_null<Tensor<X, LhsSymmetry, LhsIndexList>*> lhs_tensor,
+    const RhsTE& rhs_tensorexpression) {
   using lhs_tensorindex_list =
       tmpl::list<std::decay_t<decltype(LhsTensorIndices)>...>;
-  using rhs_tensorindex_list = typename TE::args_list;
+  using rhs_tensorindex_list = typename RhsTE::args_list;
   static_assert(
       tmpl::equal_members<lhs_tensorindex_list, rhs_tensorindex_list>::value,
       "The generic indices on the LHS of a tensor equation (that is, the "
@@ -161,38 +165,41 @@ void evaluate(const gsl::not_null<T*> lhs_tensor, const TE& rhs_te) {
           {{std::decay_t<decltype(LhsTensorIndices)>::value...}}),
       "Cannot evaluate a tensor expression to a LHS tensor with generic "
       "indices that would be contracted, e.g. evaluate<ti_A, ti_a>.");
-  using rhs_symmetry = typename TE::symmetry;
-  using rhs_tensorindextype_list = typename TE::index_list;
+  using rhs_symmetry = typename RhsTE::symmetry;
+  using rhs_tensorindextype_list = typename RhsTE::index_list;
 
   // Stores (potentially reordered) symmetry and indices expected for the LHS
   // tensor, with index order specified by LhsTensorIndices
   using lhs_tensor_symm_and_indices =
       LhsTensorSymmAndIndices<rhs_tensorindex_list, lhs_tensorindex_list,
                               rhs_symmetry, rhs_tensorindextype_list>;
-  // Instead of simply checking that `T` is the correct LHS Tensor type,
-  // individual checks for the data type, symmetry, and index_list are carried
-  // out to provide more fine-grained feedback
+  // Instead of simply checking that the LHS Tensor type is correct, individual
+  // checks for the data type, symmetry, and index_list are carried out to
+  // provide more fine-grained feedback
   static_assert(
-      std::is_same_v<typename T::type, typename TE::type>,
+      std::is_same_v<X, typename RhsTE::type>,
       "The data type stored by the LHS tensor does not match the data type "
       "stored by the RHS expression.");
   static_assert(
-      std::is_same_v<typename T::symmetry,
+      std::is_same_v<LhsSymmetry,
                      typename lhs_tensor_symm_and_indices::symmetry>,
       "The symmetry of the LHS tensor does not match the symmetry of the "
       "evaluated RHS expression.");
   static_assert(
       std::is_same_v<
-          typename T::index_list,
+          LhsIndexList,
           typename lhs_tensor_symm_and_indices::tensorindextype_list>,
       "The index list of the LHS tensor does not match the index list of the "
       "evaluated RHS expression.");
 
+  using lhs_tensor_type = typename std::decay_t<decltype(*lhs_tensor)>;
+
   // Fill LHS tensor
-  for (size_t i = 0; i < T::size(); i++) {
+  for (size_t i = 0; i < lhs_tensor_type::size(); i++) {
     (*lhs_tensor)[i] =
-        rhs_te.template get<typename T::structure,
-                            std::decay_t<decltype(LhsTensorIndices)>...>(i);
+        rhs_tensorexpression
+            .template get<typename lhs_tensor_type::structure,
+                          std::decay_t<decltype(LhsTensorIndices)>...>(i);
   }
 }
 }  // namespace TensorExpressions
