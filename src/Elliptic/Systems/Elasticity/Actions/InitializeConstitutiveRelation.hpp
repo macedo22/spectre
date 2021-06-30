2325f1c2e1d473b4396fde5d1c01e81b22e155c9
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2021-06-29 21:29:38 -0700

    Print bulk and shear modulus in InitializeConstitutiveRelation.hpp

--- src/Elliptic/Systems/Elasticity/Actions/InitializeConstitutiveRelation.hpp
+++ src/Elliptic/Systems/Elasticity/Actions/InitializeConstitutiveRelation.hpp
@@ -22,7 +22,9 @@
 #include "Elliptic/Tags.hpp"
 #include "Options/Auto.hpp"
 #include "Options/Options.hpp"
+#include "Parallel/Printf.hpp"
 #include "PointwiseFunctions/Elasticity/ConstitutiveRelations/ConstitutiveRelation.hpp"
+#include "PointwiseFunctions/Elasticity/ConstitutiveRelations/IsotropicHomogeneous.hpp"
 #include "PointwiseFunctions/Elasticity/ConstitutiveRelations/Tags.hpp"
 #include "Utilities/ErrorHandling/Error.hpp"
 #include "Utilities/TMPL.hpp"
@@ -86,6 +88,191 @@ struct ConstitutiveRelationPerBlock : db::SimpleTag,
     if (option_value.has_value()) {
       const domain::ExpandOverBlocks<ConstRelPtr> expand_over_blocks{
           block_names, block_groups};
+      // lambda for printing IsotropicHomogeneous bulk_modulus and shear_modulus
+      std::visit(
+          [](auto&& arg) {
+            // T is an alias to one of the std::variant options above:
+            // (1) ConstRelPtr, which is an alias for
+            // std::unique_ptr<ConstitutiveRelations::ConstitutiveRelation<Dim>>
+            // i.e. option_value's value is a ConstituiveRelation pointer
+            // (2) std::vector<ConstRelPtr>
+            // i.e. option_value's value is a vector of ConstitutiveRelation
+            // pointers
+            // (3) std::unordered_map<std::string, ConstRelPtr
+            // i.e. option_value's value is a map from strings to
+            // ConstitutiveRelation pointers
+            using T = std::decay_t<decltype(arg)>;
+
+            // Game plan of the code below:
+            // (1) Determine which of the three types above option_value's
+            // value is
+            // (2) Attempt to downcast the ConstitutiveRelation part of the type
+            // to an IsotropicHomogeneous type. If it succeeeds, then we know
+            // option_value is (or holds a vector or map of)
+            // IsotropicHomogeneous, as opposed to a CubicCrystal, which is
+            // another derived type of ConstitutiveRelation, just like
+            // IsotropicHomogeneous. In short, gotta make sure we are dealing
+            // with an IsotropicHomogeneous thing.
+            // (3) If we do have an IsotropicHomogeneous thing, then print
+            // the bulk modulus/moduli and shear modulus/moduli. If
+            // option_value's value is a vector or map, multiple are printed.
+
+            // if option_value's value is a ConstitutiveRelation pointer
+            if constexpr (std::is_same_v<T, ConstRelPtr>) {
+              // attempt to downcast option_value's type from base type
+              // ConstitutiveRelation to derived type IsotropicHomogeneous
+              const auto derived_arg = dynamic_cast<
+                  ConstitutiveRelations::IsotropicHomogeneous<Dim>*>(&(*arg));
+              // if the cast was successful
+              if (derived_arg) {
+                // If the program enters this if block, then it means that
+                // option_value's value is of type IsotropicHomogeneous
+                // and derived_arg is an IsotropicHomogeneous pointer
+
+                // call IsotropicHomogeneous bulk_modulus and shear_modulus
+                // member functions to get the bulk modulus and shear modulus
+                // and print the values
+                Parallel::printf("bulk_modulus: %f, shear_modulus: %f\n\n",
+                                 derived_arg->bulk_modulus(),
+                                 derived_arg->shear_modulus());
+              }
+            }
+            // otherwise, if option_value's value is a vector of
+            // ConstitutiveRelation pointers
+            else if constexpr (std::is_same_v<T, std::vector<ConstRelPtr>>) {
+              // An iterator to iterate over the option_value vector. The next
+              // line assigns it to refer to the first element of the vector.
+              auto arg_it = arg.begin();
+              // if the vector is not empty (i.e. has at least one element)
+              if (arg_it != arg.end()) {
+                // attempt to downcast the first ConstitutiveRelation pointer
+                // element in the vector to an IsotropicHeomogeneous pointer
+                const auto first_derived_arg_value = dynamic_cast<
+                    ConstitutiveRelations::IsotropicHomogeneous<Dim>*>(
+                    &(*(*arg_it)));
+                // if the cast was successful
+                if (first_derived_arg_value) {
+                  // If the program enters this if block, then it means that
+                  // option_value's first element is an IsotropicHomogeneous
+                  // pointer. Note that no code below checks the other elements,
+                  // it just assumes that if the first element is a pointer to
+                  // an IsotropicHomogeneous object, the others are, too.
+
+                  // call IsotropicHomogeneous bulk_modulus and shear_modulus
+                  // member functions to get the bulk modulus and shear modulus
+                  // of the IsotropicHomogenous object pointed to by the first
+                  // pointer in the vector, then print their values
+                  Parallel::printf(
+                      "The modulus values are:\nbulk_modulus: %f, "
+                      "shear_modulus: %f\n",
+                      first_derived_arg_value->bulk_modulus(),
+                      first_derived_arg_value->shear_modulus());
+                  // move iterator to the next pointer in the vector
+                  arg_it++;
+                  // while we haven't reached the end of the vector
+                  // (i.e. we still have more elements to print stuff about)
+                  while (arg_it != arg.end()) {
+                    // attempt to downcast this ConstitutiveRelation pointer
+                    // element in the vector to an IsotropicHeomogeneous pointer
+                    const auto derived_arg_value = dynamic_cast<
+                        ConstitutiveRelations::IsotropicHomogeneous<Dim>*>(
+                        &(*(*arg_it)));
+                    // call IsotropicHomogeneous bulk_modulus and shear_modulus
+                    // member functions to get the bulk modulus and shear
+                    // modulus of the IsotropicHomogenous object pointed to by
+                    // this pointer in the vector, then print their values
+                    Parallel::printf("bulk_modulus: %f, shear_modulus: %f\n",
+                                     derived_arg_value->bulk_modulus(),
+                                     derived_arg_value->shear_modulus());
+                    // move iterator to the next pointer in the vector
+                    arg_it++;
+                  }
+                  Parallel::printf("\n");
+                }
+              }
+            }
+            // otherwise, if option_value's value is a map that maps strings to
+            // ConstitutiveRelation pointers
+            else if constexpr (std::is_same_v<
+                                   T, std::unordered_map<std::string,
+                                                         ConstRelPtr>>) {
+              // Notes about this map:
+              //
+              // arg is a map where each element of the map is a key-value pair,
+              // the keys being strings and the values that they map to being
+              // ConstitutiveRelation pointers. If you have one element of the
+              // map you can access the key with `first` and the value with
+              // `second`, e.g. if `e` is one element (key-value pair) of our
+              // map, then:
+              //     std::string e_key = e.first;
+              //     ConstRelPtr e_value = e.second;
+
+              // An iterator to iterate over the option_value map. The next
+              // line assigns it to refer to the first element (key-value pair)
+              // of the map.
+              auto arg_it = arg.begin();
+
+              // if the map is not empty (i.e. has at least one element)
+              if (arg_it != arg.end()) {
+                // attempt to downcast the first ConstitutiveRelation pointer
+                // value in the map to an IsotropicHomogeneous pointer
+                auto first_derived_arg_value = dynamic_cast<
+                    ConstitutiveRelations::IsotropicHomogeneous<Dim>*>(
+                    &(*(arg_it->second)));
+                // if the cast was successful
+                if (first_derived_arg_value) {
+                  // If the program enters this if block, then it means that
+                  // option_value's first value is an IsotropicHomogeneous
+                  // pointer. Note that no code below checks the other elements,
+                  // it just assumes that if the first element's value is a
+                  // pointer to an IsotropicHomogeneous object, the others are,
+                  // too.
+
+                  // call IsotropicHomogeneous bulk_modulus and shear_modulus
+                  // member functions to get the bulk modulus and shear modulus
+                  // of the IsotropicHomogenous object pointed to by the first
+                  // pointer in the map, then print their values
+                  Parallel::printf(
+                      "The modulus values are:\nkey: %s, bulk_modulus: %f, "
+                      "shear_modulus: %f\n",
+                      arg_it->first, first_derived_arg_value->bulk_modulus(),
+                      first_derived_arg_value->shear_modulus());
+                  // move iterator to the next element in the map
+                  arg_it++;
+                  // while we haven't reached the end of the map
+                  // (i.e. we still have more elements to print stuff about)
+                  while (arg_it != arg.end()) {
+                    // attempt to downcast this ConstitutiveRelation pointer
+                    // in the map to an IsotropicHeomogeneous pointer
+                    auto derived_arg_value = dynamic_cast<
+                        ConstitutiveRelations::IsotropicHomogeneous<Dim>*>(
+                        &(*(arg_it->second)));
+                    // call IsotropicHomogeneous bulk_modulus and shear_modulus
+                    // member functions to get the bulk modulus and shear
+                    // modulus of the IsotropicHomogenous object pointed to by
+                    // this pointer in the map, then print their values
+                    Parallel::printf(
+                        "key: %s, bulk_modulus: %f, shear_modulus: %f\n",
+                        arg_it->first, derived_arg_value->bulk_modulus(),
+                        derived_arg_value->shear_modulus());
+                    // move iterator to the next element in the map
+                    arg_it++;
+                  }
+                  Parallel::printf("\n");
+                }
+              }
+            }
+            // otherwise, option_value's value is none of the three type options
+            // previously mentioned above
+            else {
+              // If you get here, it means there is a mistake somewhere in the
+              // code, because it doesn't seem like it should be anything else
+
+              // pls come up with a better error message lol
+              ERROR("shouldn't get here");
+            }
+          },  // end lambda
+          option_value.value());
       try {
         return std::visit(expand_over_blocks, *option_value);
       } catch (const std::exception& error) {
