ecde4fc8fdb0ba7cacfef6fc21d63f81ab7c61bb
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2021-10-12 16:37:44 -0700

    Add inner product cases

---
+++ tests/Unit/DataStructures/Tensor/Expressions/CompileTime/InnerProduct/3x3to0/Dim3/Double/Test_InnerProduct3x3to0Dim3Double.cpp
@@ -0,0 +1,42 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <climits>
+#include <cstddef>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace {
+template <size_t Dim, typename Generator, typename DataType>
+void test(const gsl::not_null<Generator*> generator,
+          const DataType& used_for_size) {
+  std::uniform_real_distribution<> distribution(-1.0, 1.0);
+
+  const auto R =
+      make_with_random_values<tnsr::AbC<DataType, Dim, Frame::Inertial>>(
+          generator, make_not_null(&distribution), used_for_size);
+
+  const auto S =
+      make_with_random_values<tnsr::Abc<DataType, Dim, Frame::Inertial>>(
+          generator, make_not_null(&distribution), used_for_size);
+
+  Scalar<DataType> L(used_for_size);
+
+  TensorExpressions::evaluate(make_not_null(&L),
+                              R(ti_A, ti_b, ti_C) * S(ti_B, ti_c, ti_a));
+}
+}  // namespace
+
+SPECTRE_TEST_CASE(
+    "Unit.DataStructures.Tensor.Expression.CompileTime.InnerProduct.3x3to0Dim3Double",
+    "[DataStructures][Unit]") {
+  MAKE_GENERATOR(generator);
+
+  test<3>(make_not_null(&generator),
+          std::numeric_limits<double>::signaling_NaN());
+}
---
+++ tests/Unit/DataStructures/Tensor/Expressions/CompileTime/InnerProduct/4x4to0/Dim3/Double/Test_InnerProduct4x4to0Dim3Double.cpp
@@ -0,0 +1,50 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <climits>
+#include <cstddef>
+
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
+#include "Utilities/Gsl.hpp"
+
+namespace {
+template <size_t Dim, typename Generator, typename DataType>
+void test(const gsl::not_null<Generator*> generator,
+          const DataType& used_for_size) {
+  std::uniform_real_distribution<> distribution(-1.0, 1.0);
+
+  const auto R = make_with_random_values<
+      Tensor<DataType, Symmetry<4, 3, 2, 1>,
+             index_list<SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>,
+                        SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>,
+                        SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>,
+                        SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>>>>(
+      generator, make_not_null(&distribution), used_for_size);
+
+  const auto S = make_with_random_values<
+      Tensor<DataType, Symmetry<4, 3, 2, 1>,
+             index_list<SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>,
+                        SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>,
+                        SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>,
+                        SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>>>>(
+      generator, make_not_null(&distribution), used_for_size);
+
+  Scalar<DataType> L(used_for_size);
+
+  TensorExpressions::evaluate(
+      make_not_null(&L), R(ti_A, ti_b, ti_c, ti_D) * S(ti_B, ti_d, ti_C, ti_a));
+}
+}  // namespace
+
+SPECTRE_TEST_CASE(
+    "Unit.DataStructures.Tensor.Expression.CompileTime.InnerProduct.4x4to0Dim3Double",
+    "[DataStructures][Unit]") {
+  MAKE_GENERATOR(generator);
+
+  test<3>(make_not_null(&generator),
+          std::numeric_limits<double>::signaling_NaN());
+}
