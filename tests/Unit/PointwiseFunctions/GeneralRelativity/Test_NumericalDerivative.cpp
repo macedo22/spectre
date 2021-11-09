81af0539a239994f5f7042315c0a982c9f780b55
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2021-11-09 15:18:35 -0800

    Test with 1D

--- tests/Unit/PointwiseFunctions/GeneralRelativity/Test_NumericalDerivative.cpp
+++ tests/Unit/PointwiseFunctions/GeneralRelativity/Test_NumericalDerivative.cpp
@@ -21,8 +21,6 @@
 #include "Domain/CoordinateMaps/Affine.hpp"
 #include "Domain/CoordinateMaps/CoordinateMap.hpp"
 #include "Domain/CoordinateMaps/CoordinateMap.tpp"
-#include "Domain/CoordinateMaps/ProductMaps.hpp"
-#include "Domain/CoordinateMaps/ProductMaps.tpp"
 #include "Domain/LogicalCoordinates.hpp"
 #include "Framework/TestHelpers.hpp"
 #include "Helpers/PointwiseFunctions/AnalyticSolutions/TestHelpers.hpp"
@@ -39,7 +37,6 @@
 
 namespace {
 using Affine = domain::CoordinateMaps::Affine;
-using Affine3D = domain::CoordinateMaps::ProductOf3Maps<Affine, Affine, Affine>;
 
 // Tag for the function we want to differentiate: F = 1 / r
 template <typename DataType>
@@ -64,20 +61,15 @@ tnsr::i<DataType, Dim, FrameType> get_deriv_one_over_radius(
   return deriv_one_over_radius;
 }
 
-template <size_t SpatialDim, typename FrameType, Spectral::Basis Basis,
+template <typename FrameType, Spectral::Basis Basis,
           Spectral::Quadrature Quadrature>
-double get_l2norm(const size_t num_points_1d,
-                  const std::array<double, 3>& lower_bound,
-                  const std::array<double, 3>& upper_bound) {
+double get_l2norm(const size_t num_points, const double lower_bound,
+                  const double upper_bound) {
   // Setup grid
-  Mesh<SpatialDim> mesh{num_points_1d, Basis, Quadrature};
+  Mesh<1 /*SpatialDim*/> mesh{num_points, Basis, Quadrature};
   const auto coord_map =
-      domain::make_coordinate_map<Frame::ElementLogical, FrameType>(Affine3D{
-          Affine{-1., 1., lower_bound[0], upper_bound[0]},
-          Affine{-1., 1., lower_bound[1], upper_bound[1]},
-          Affine{-1., 1., lower_bound[2], upper_bound[2]},
-      });
-  const size_t num_points_3d = num_points_1d * num_points_1d * num_points_1d;
+      domain::make_coordinate_map<Frame::ElementLogical, FrameType>(
+          Affine{-1., 1., lower_bound, upper_bound});
   // Setup coordinates
   const auto x_logical = logical_coordinates(mesh);
   const auto x = coord_map(x_logical);
@@ -91,12 +83,12 @@ double get_l2norm(const size_t num_points_1d,
 
   // Compute actual numerical derivative of the determinant
   using F_tag = ::F<DataVector>;
-  Variables<tmpl::list<F_tag>> F_var(num_points_3d);
+  Variables<tmpl::list<F_tag>> F_var(num_points);
   get<F_tag>(F_var) = F;
   const auto numerical_deriv_F_var = partial_derivatives<tmpl::list<F_tag>>(
       F_var, mesh, coord_map.inv_jacobian(x_logical));
   const auto& numerical_deriv_F =
-      get<Tags::deriv<F_tag, tmpl::size_t<SpatialDim>, FrameType>>(
+      get<Tags::deriv<F_tag, tmpl::size_t<1 /*SpatialDim*/>, FrameType>>(
           numerical_deriv_F_var);
 
   const double max_diff =
@@ -111,31 +103,27 @@ double get_l2norm(const size_t num_points_1d,
 
 template <Spectral::Basis Basis, Spectral::Quadrature Quadrature>
 void test() {
-  const size_t dim = 3;
   using frame = Frame::Inertial;
 
-  const std::array<double, 3> lower_bound{{0.8, 1.22, 1.3}};
+  const double lower_bound = 0.8;
   const std::array<double, 5> box_lengths{{0.03125, 0.0625, 0.125, 0.25, 0.5}};
-  const std::array<size_t, 5> num_points_1d{{4, 6, 8, 10, 12}};
+  const std::array<size_t, 5> num_points{{4, 6, 8, 10, 12}};
   // Table of L2 norms of difference between analytical and numerical
   // derivatives for each box length and # 1D grid points
-  std::array<std::array<double, box_lengths.size()>, num_points_1d.size()>
+  std::array<std::array<double, box_lengths.size()>, num_points.size()>
       l2norms{};
 
   std::cout << "==== L2 norm ====" << std::endl;
-  for (size_t i = 0; i < num_points_1d.size(); i++) {
-    std::cout << "Num points 1D : " << num_points_1d[i] << std::endl
-              << std::endl;
+  for (size_t i = 0; i < num_points.size(); i++) {
+    std::cout << "Num points 1D : " << num_points[i] << std::endl << std::endl;
     for (size_t n = 0; n < box_lengths.size(); n++) {
       const double length = box_lengths[n];
-      const std::array<double, 3> upper_bound{{lower_bound[0] + length,
-                                               lower_bound[1] + length,
-                                               lower_bound[2] + length}};
+      const double upper_bound = lower_bound + length;
       std::cout << "Length : " << length << std::endl;
       std::cout << "lower bound : " << lower_bound << std::endl;
       std::cout << "upper bound : " << upper_bound << std::endl;
-      const double l2norm = get_l2norm<dim, frame, Basis, Quadrature>(
-          num_points_1d[i], lower_bound, upper_bound);
+      const double l2norm = get_l2norm<frame, Basis, Quadrature>(
+          num_points[i], lower_bound, upper_bound);
       l2norms[i][n] = l2norm;
       std::cout << std::endl;
     }
@@ -144,19 +132,16 @@ void test() {
   std::cout << "====================================================="
             << std::endl;
 
-  std::array<std::array<double, box_lengths.size() - 1>, num_points_1d.size()>
+  std::array<std::array<double, box_lengths.size() - 1>, num_points.size()>
       convergence_orders{};
   std::cout << "\n==== L2 norm convergence orders ====" << std::endl;
-  for (size_t i = 0; i < num_points_1d.size(); i++) {
-    std::cout << "Num points 1D : " << num_points_1d[i] << std::endl
-              << std::endl;
+  for (size_t i = 0; i < num_points.size(); i++) {
+    std::cout << "Num points 1D : " << num_points[i] << std::endl << std::endl;
 
     for (size_t n = 1; n < box_lengths.size(); n++) {
       const double length = box_lengths[n];
       const double half_length = box_lengths[n - 1];
-      const std::array<double, 3> upper_bound{{lower_bound[0] + length,
-                                               lower_bound[1] + length,
-                                               lower_bound[2] + length}};
+      const double upper_bound = lower_bound + length;
       std::cout << "Length : " << length << std::endl;
       std::cout << "lower bound : " << lower_bound << std::endl;
       std::cout << "upper bound : " << upper_bound << std::endl;
