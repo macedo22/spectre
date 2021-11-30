5a69ac3c35eb4eb1a941faeede98da90e78a3013
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2021-11-30 13:40:41 -0800

    Test runtime of some inner product cases
    The difference between the times for TEs and loops don't seem to differ by much. Maybe this is because the code that sets up the tensors dominates the runtime?

---
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_InnerProductDataVectorLoop.cpp
@@ -0,0 +1,62 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <cstddef>
+#include <limits>
+#include <random>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeWithValue.hpp"
+
+SPECTRE_TEST_CASE(
+    "Unit.DataStructures.Tensor.Expression.InnerProductDataVectorLoop",
+    "[Unit][DataStructures]") {
+  // User runtime using $ time ./bin/RunSingleTest
+  // 2 x 2 spatial : 0m0.027s
+  // 3 x 3 spatial : 0m0.040s
+  // 3 (+ 1) x 3 (+ 1) spacetime : 0m0.129s
+
+  MAKE_GENERATOR(generator);
+  std::uniform_real_distribution<> distribution(0.1, 1.0);
+
+  using DataType = DataVector;
+  constexpr size_t Dim = 3;
+  using FrameType = Frame::Inertial;
+  using R_type = Tensor<DataType, Symmetry<4, 3, 2, 1>,
+                        index_list<SpacetimeIndex<Dim, UpLo::Up, FrameType>,
+                                   SpacetimeIndex<Dim, UpLo::Up, FrameType>,
+                                   SpacetimeIndex<Dim, UpLo::Up, FrameType>,
+                                   SpacetimeIndex<Dim, UpLo::Up, FrameType>>>;
+  using S_type = Tensor<DataType, Symmetry<4, 3, 2, 1>,
+                        index_list<SpacetimeIndex<Dim, UpLo::Lo, FrameType>,
+                                   SpacetimeIndex<Dim, UpLo::Lo, FrameType>,
+                                   SpacetimeIndex<Dim, UpLo::Lo, FrameType>,
+                                   SpacetimeIndex<Dim, UpLo::Lo, FrameType>>>;
+
+  constexpr size_t num_points = 1000;
+  const DataVector used_for_size =
+      DataVector(num_points, std::numeric_limits<double>::signaling_NaN());
+
+  const auto R = make_with_random_values<R_type>(make_not_null(&generator),
+                                                 distribution, used_for_size);
+  const auto S = make_with_random_values<S_type>(make_not_null(&generator),
+                                                 distribution, used_for_size);
+
+  auto L = make_with_value<Scalar<DataType>>(used_for_size, 0.0);
+
+  for (size_t i = 0; i < Dim + 1; i++) {
+    for (size_t j = 0; j < Dim + 1; j++) {
+      for (size_t k = 0; k < Dim + 1; k++) {
+        for (size_t l = 0; l < Dim + 1; l++) {
+          get(L) += R.get(i, j, k, l) * S.get(i, j, k, l);
+        }
+      }
+    }
+  }
+}
