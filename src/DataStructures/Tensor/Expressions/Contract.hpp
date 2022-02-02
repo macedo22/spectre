047860667e29d53af0bd603d897da1d51ef61cdb
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-02-01 21:08:47 -0800

    Increase TensorContract max number of operands in subtree

--- src/DataStructures/Tensor/Expressions/Contract.hpp
+++ src/DataStructures/Tensor/Expressions/Contract.hpp
@@ -297,8 +297,8 @@ struct TensorContract
   static constexpr size_t num_ops_to_evaluate_main_right = num_ops_right;
   static constexpr size_t num_ops_to_evaluate_main_subtree =
       num_ops_to_evaluate_main_left + num_ops_to_evaluate_main_right;
-  static constexpr bool is_main_beg =
-      num_ops_to_evaluate_main_subtree >= detail::max_num_ops_in_sub_expression;
+  static constexpr bool is_main_beg = num_ops_to_evaluate_main_subtree >=
+                                      2 * detail::max_num_ops_in_sub_expression;
 
   explicit TensorContract(
       const TensorExpression<T, X, Symm, IndexList, ArgsList>& t)
