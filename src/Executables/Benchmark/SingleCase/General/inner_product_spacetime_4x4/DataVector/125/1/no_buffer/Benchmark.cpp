602d077757335d158c979fb6697314e69bb5c377
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-02-03 20:24:03 -0800

    Add SingleCase cases for TE spacetime inner products ranks 1 to 4

---
+++ src/Executables/Benchmark/SingleCase/General/inner_product_spacetime_4x4/DataVector/125/1/no_buffer/Benchmark.cpp
@@ -0,0 +1,77 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wredundant-decls"
+#include <benchmark.h>
+#pragma GCC diagnostic pop
+#include <random>
+
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "Executables/Benchmark/BenchmarkHelpers.hpp"
+#include "Helpers/DataStructures/MakeWithRandomValues.hpp"
+#include "Utilities/Gsl.hpp"
+
+// Charm looks for this function but since we build without a main function or
+// main module we just have it be empty
+extern "C" void CkRegisterMainModule(void) {}
+
+namespace {
+constexpr size_t seed = 17;
+std::mt19937 generator(seed);
+
+// tensor types in tensor equation being benchmarked
+using DataType = DataVector;
+static constexpr size_t Dim = 1;
+constexpr size_t num_grid_points = 125;
+
+using R_type =
+    Tensor<DataType, Symmetry<4, 3, 2, 1>,
+           index_list<SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>,
+                      SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>,
+                      SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>,
+                      SpacetimeIndex<Dim, UpLo::Up, Frame::Inertial>>>;
+using S_type =
+    Tensor<DataType, Symmetry<4, 3, 2, 1>,
+           index_list<SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>,
+                      SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>,
+                      SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>,
+                      SpacetimeIndex<Dim, UpLo::Lo, Frame::Inertial>>>;
+using result_type = Scalar<DataType>;
+
+void bench(benchmark::State& state) {  // NOLINT
+  const DataType used_for_size =
+      BenchmarkHelpers::get_used_for_size<DataType>(num_grid_points);
+  std::uniform_real_distribution<> distribution(0.1, 1.0);
+
+  // RHS: R
+  const R_type R = make_with_random_values<R_type>(
+      make_not_null(&generator), make_not_null(&distribution), used_for_size);
+
+  // RHS: S
+  const S_type S = make_with_random_values<S_type>(
+      make_not_null(&generator), make_not_null(&distribution), used_for_size);
+
+  // LHS: result
+  result_type result(used_for_size);
+
+  for (auto _ : state) {
+    // LHS: result
+    TensorExpressions::evaluate(
+        make_not_null(&result),
+        R(ti_A, ti_B, ti_C, ti_D) * S(ti_a, ti_b, ti_c, ti_d));
+    benchmark::DoNotOptimize(result);
+    benchmark::ClobberMemory();
+  }
+}
+
+BENCHMARK(bench);
+}  // namespace
+
+// Ignore the warning about an extra ';' because some versions of benchmark
+// require it
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+BENCHMARK_MAIN();
+#pragma GCC diagnostic pop
