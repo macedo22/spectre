1277b315cd31e6f8615a06a31bfea3c0a33cfb0f
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-02-05 21:51:35 -0800

    SPECTRE_ALWAYS_INLINE all the things

--- src/DataStructures/Tensor/Expressions/AddSubtract.hpp
+++ src/DataStructures/Tensor/Expressions/AddSubtract.hpp
@@ -348,7 +348,7 @@ struct AddSub<T1, T2, ArgsList1<Args1...>, ArgsList2<Args2...>, Sign>
   AddSub(T1 t1, T2 t2) : t1_(std::move(t1)), t2_(std::move(t2)) {}
   ~AddSub() override = default;
 
-  type get_used_for_size() const {
+  SPECTRE_ALWAYS_INLINE type get_used_for_size() const {
     if constexpr (not std::is_base_of_v<NumberAsExpression, T2>) {
       return t2_.get_used_for_size();
     } else {
