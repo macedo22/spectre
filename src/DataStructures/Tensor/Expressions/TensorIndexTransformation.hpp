946ac42f5b7d1ad2a195675c5b82bf55422649bf
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-02-07 15:17:58 -0800

    Switch order of ternary expression in TensorIndexTransformation

--- src/DataStructures/Tensor/Expressions/TensorIndexTransformation.hpp
+++ src/DataStructures/Tensor/Expressions/TensorIndexTransformation.hpp
@@ -151,11 +151,10 @@ transform_multi_index(
       make_array<NumIndicesOut, size_t>(0);
   for (size_t i = 0; i < NumIndicesOut; i++) {
     gsl::at(output_multi_index, i) =
-        (gsl::at(tensorindex_transformation, i) ==
+        (gsl::at(tensorindex_transformation, i) !=
          TensorIndexTransformation_detail::time_index_position_placeholder)
-            ? 0
-            : gsl::at(input_multi_index,
-                      gsl::at(tensorindex_transformation, i));
+            ? gsl::at(input_multi_index, gsl::at(tensorindex_transformation, i))
+            : 0;
   }
   return output_multi_index;
 }
