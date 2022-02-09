5c1b714028fc2e3d6ad7c3991313f5ca32e60b9c
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-02-08 17:06:51 -0800

    Remove unnecessary detail::get_used_for_size in Evaluate.hpp

--- src/DataStructures/Tensor/Expressions/Evaluate.hpp
+++ src/DataStructures/Tensor/Expressions/Evaluate.hpp
@@ -97,11 +97,6 @@ SPECTRE_ALWAYS_INLINE constexpr bool is_evaluated_lhs_multi_index(
     return true;
   }
 }
-
-template <typename RhsTE>
-typename RhsTE::type get_used_for_size(const RhsTE& rhs_tensorexpression) {
-  return rhs_tensorexpression.get_used_for_size();
-}
 }  // namespace detail
 
 /*!
@@ -320,7 +315,7 @@ auto evaluate(const RhsTE& rhs_tensorexpression) {
 
   Tensor<typename RhsTE::type, typename lhs_tensor_symm_and_indices::symmetry,
          typename lhs_tensor_symm_and_indices::tensorindextype_list>
-      lhs_tensor(detail::get_used_for_size(rhs_tensorexpression));
+      lhs_tensor(rhs_tensorexpression.get_used_for_size());
   evaluate<LhsTensorIndices...>(make_not_null(&lhs_tensor),
                                 rhs_tensorexpression);
   return lhs_tensor;
