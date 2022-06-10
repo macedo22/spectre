8dfbd9916a1f643aa8505c4a29608ef4987d80d7
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-06-09 20:53:06 -0700

    dynamic_cast to r-value

--- src/Utilities/ErrorHandling/Error.hpp
+++ src/Utilities/ErrorHandling/Error.hpp
@@ -55,7 +55,7 @@
       disable_floating_point_exceptions();                                   \
       abort_with_error_message(                                              \
           __FILE__, __LINE__, static_cast<const char*>(__PRETTY_FUNCTION__), \
-          static_cast<const std::ostringstream&>(std::ostringstream() << m)  \
+          dynamic_cast<std::ostringstream&&>(std::ostringstream() << m)      \
               .str());                                                       \
     }                                                                        \
   } while (false)
@@ -73,7 +73,7 @@
       disable_floating_point_exceptions();                                   \
       abort_with_error_message_no_trace(                                     \
           __FILE__, __LINE__, static_cast<const char*>(__PRETTY_FUNCTION__), \
-          static_cast<const std::ostringstream&>(std::ostringstream() << m)  \
+          dynamic_cast<std::ostringstream&&>(std::ostringstream() << m)      \
               .str());                                                       \
     }                                                                        \
   } while (false)
