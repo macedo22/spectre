eb5cb425a314823c4ff22a970883224ec36beb29
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-06-29 19:27:27 -0700

    fixup! Add derived TensorExpression for Kronecker delta

--- tests/Unit/DataStructures/Tensor/Expressions/Test_KroneckerDelta.cpp
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_KroneckerDelta.cpp
@@ -8,30 +8,38 @@
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "Framework/TestHelpers.hpp"
 
-SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.KroneckerDelta",
-                  "[DataStructures][Unit]") {
-  static_assert(kdelta1.dim == 1, "kdelta1 dimension should be 1");
-  static_assert(kdelta2.dim == 2, "kdelta2 dimension should be 2");
-  static_assert(kdelta3.dim == 3, "kdelta3 dimension should be 3");
-  static_assert(kdelta4.dim == 4, "kdelta4 dimension should be 4");
+namespace {
+template <typename Frame>
+void test_kronecker_delta_properties() {
+  // check dimension value
+  static_assert(kronecker_delta<1, Frame>.dim == 1,
+                "kronecker_delta<1> dimension should be 1");
+  static_assert(kronecker_delta<2, Frame>.dim == 2,
+                "kronecker_delta<2> dimension should be 2");
+  static_assert(kronecker_delta<3, Frame>.dim == 3,
+                "kronecker_delta<3> dimension should be 3");
+  static_assert(kronecker_delta<4, Frame>.dim == 4,
+                "kronecker_delta<4> dimension should be 4");
+
+  // check component values
 
   // dimension 1
-  const auto kdeltaIj = kdelta1(ti::I, ti::j);
+  const auto kdeltaIj = kronecker_delta<1, Frame>(ti::I, ti::j);
   CHECK(kdeltaIj.get({{0}}) == 1.0);
 
   // dimension 2
-  const auto kdeltakJ = kdelta2(ti::k, ti::J);
+  const auto kdeltakJ = kronecker_delta<2, Frame>(ti::k, ti::J);
   CHECK(kdeltakJ.get({{0, 0}}) == 1.0);
   CHECK(kdeltakJ.get({{1, 0}}) == 0.0);
   CHECK(kdeltakJ.get({{0, 1}}) == 0.0);
   CHECK(kdeltakJ.get({{1, 1}}) == 1.0);
 
   // dimension 3 contracted
-  const auto kdeltaJj = kdelta3(ti::J, ti::j);
+  const auto kdeltaJj = kronecker_delta<3, Frame>(ti::J, ti::j);
   CHECK(kdeltaJj.get({{}}) == 3.0);
 
   // dimension 4, spacetime
-  const auto kdeltaAb = kdelta4(ti::A, ti::b);
+  const auto kdeltaAb = kronecker_delta<4, Frame>(ti::A, ti::b);
   for (size_t a = 0; a < 4; a++) {
     for (size_t b = 0; b < 4; b++) {
       if (a == b) {
@@ -41,4 +49,11 @@ SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.KroneckerDelta",
       }
     }
   }
+}
+}  // namespace
+
+SPECTRE_TEST_CASE("Unit.DataStructures.Tensor.Expression.KroneckerDelta",
+                  "[DataStructures][Unit]") {
+  test_kronecker_delta_properties<Frame::Inertial>();
+  test_kronecker_delta_properties<Frame::Grid>();
 }
