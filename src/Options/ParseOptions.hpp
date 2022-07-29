4ea2d37c8db59040239c49adb4b32fa5531d23e6
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-07-29 08:12:42 -0700

    Remove the lambda from create_from_yaml::create(Options)

--- src/Options/ParseOptions.hpp
+++ src/Options/ParseOptions.hpp
@@ -1018,33 +1018,6 @@ T create_from_yaml<T>::create(const Option& options) {
   Parser<typename Options_detail::get_options_list<T, Metavariables>::type>
       parser(T::help);
   parser.parse(options);
-  return parser.template apply_all<Metavariables>([&options](
-                                                      auto parsed_options,
-                                                      auto&&... args) {
-    if constexpr (std::is_constructible<T, decltype(parsed_options),
-                                        decltype(std::move(args))...,
-                                        const Context&, Metavariables>{}) {
-      return T(parsed_options, std::move(args)..., options.context(),
-               Metavariables{});
-    } else if constexpr (std::is_constructible<T, decltype(parsed_options),
-                                               decltype(std::move(args))...,
-                                               const Context&>{}) {
-      return T(parsed_options, std::move(args)..., options.context());
-    } else if constexpr (std::is_constructible<T, decltype(parsed_options),
-                                               decltype(
-                                                   std::move(args))...>{}) {
-      return T(parsed_options, std::move(args)...);
-    } else if constexpr (std::is_constructible<T, decltype(std::move(args))...,
-                                               const Context&,
-                                               Metavariables>{}) {
-      return T(std::move(args)..., options.context(), Metavariables{});
-    } else if constexpr (std::is_constructible<T, decltype(std::move(args))...,
-                                               const Context&>{}) {
-      return T(std::move(args)..., options.context());
-    } else {
-      return T{std::move(args)...};
-    }
-  });
 }
 
 // yaml-cpp doesn't handle C++11 types yet
