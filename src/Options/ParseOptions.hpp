cb470d43c9cd12abeaec75cb9662abed834fe089
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-07-29 02:56:50 -0700

    Comment out body of the unspecialized create_from_yaml::create

--- src/Options/ParseOptions.hpp
+++ src/Options/ParseOptions.hpp
@@ -1016,36 +1016,37 @@ struct get_options_list<T, Metavariables, std::void_t<typename T::options>> {
 template <typename T>
 template <typename Metavariables>
 T create_from_yaml<T>::create(const Option& options) {
-  Parser<typename Options_detail::get_options_list<T, Metavariables>::type>
-      parser(T::help);
-  parser.parse(options);
-  return parser.template apply_all<Metavariables>([&options](
-                                                      auto parsed_options,
-                                                      auto&&... args) {
-    if constexpr (std::is_constructible<T, decltype(parsed_options),
-                                        decltype(std::move(args))...,
-                                        const Context&, Metavariables>{}) {
-      return T(parsed_options, std::move(args)..., options.context(),
-               Metavariables{});
-    } else if constexpr (std::is_constructible<T, decltype(parsed_options),
-                                               decltype(std::move(args))...,
-                                               const Context&>{}) {
-      return T(parsed_options, std::move(args)..., options.context());
-    } else if constexpr (std::is_constructible<T, decltype(parsed_options),
-                                               decltype(
-                                                   std::move(args))...>{}) {
-      return T(parsed_options, std::move(args)...);
-    } else if constexpr (std::is_constructible<T, decltype(std::move(args))...,
-                                               const Context&,
-                                               Metavariables>{}) {
-      return T(std::move(args)..., options.context(), Metavariables{});
-    } else if constexpr (std::is_constructible<T, decltype(std::move(args))...,
-                                               const Context&>{}) {
-      return T(std::move(args)..., options.context());
-    } else {
-      return T{std::move(args)...};
-    }
-  });
+  return T{};
+  // Parser<typename Options_detail::get_options_list<T, Metavariables>::type>
+  //     parser(T::help);
+  // parser.parse(options);
+  // return parser.template apply_all<Metavariables>([&options](
+  //                                                     auto parsed_options,
+  //                                                     auto&&... args) {
+  //   if constexpr (std::is_constructible<T, decltype(parsed_options),
+  //                                       decltype(std::move(args))...,
+  //                                       const Context&, Metavariables>{}) {
+  //     return T(parsed_options, std::move(args)..., options.context(),
+  //              Metavariables{});
+  //   } else if constexpr (std::is_constructible<T, decltype(parsed_options),
+  //                                              decltype(std::move(args))...,
+  //                                              const Context&>{}) {
+  //     return T(parsed_options, std::move(args)..., options.context());
+  //   } else if constexpr (std::is_constructible<T, decltype(parsed_options),
+  //                                              decltype(
+  //                                                  std::move(args))...>{}) {
+  //     return T(parsed_options, std::move(args)...);
+  //   } else if constexpr (std::is_constructible<T, decltype(std::move(args))...,
+  //                                              const Context&,
+  //                                              Metavariables>{}) {
+  //     return T(std::move(args)..., options.context(), Metavariables{});
+  //   } else if constexpr (std::is_constructible<T, decltype(std::move(args))...,
+  //                                              const Context&>{}) {
+  //     return T(std::move(args)..., options.context());
+  //   } else {
+  //     return T{std::move(args)...};
+  //   }
+  // });
 }
 
 // yaml-cpp doesn't handle C++11 types yet
@@ -1127,8 +1128,8 @@ struct YAML::convert<Options::Options_detail::CreateWrapper<T, Metavariables>> {
     context.top_level = false;
     context.append("While creating a " + pretty_type::name<T>());
     Options::Option options(node, std::move(context));
-    // rhs = Options::Options_detail::CreateWrapper<T, Metavariables>{
-    //     Options::create_from_yaml<T>::template create<Metavariables>(options)};
+    rhs = Options::Options_detail::CreateWrapper<T, Metavariables>{
+        Options::create_from_yaml<T>::template create<Metavariables>(options)};
     return true;
   }
 };
