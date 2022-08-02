4864230a008bdb6102fb1920b9f3563beecfc237
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-08-01 20:39:34 -0700

    Replace options_list with empty tmpl::list<> in first call to Options::apply in Parallel::Main
    Note that the 2nd call was also commented out. Both calls need to be commented out to get the compilation saves, so by commenting one out and leaving one, we can edit the only existent one to see how things change.

--- src/Parallel/Main.hpp
+++ src/Parallel/Main.hpp
@@ -362,7 +362,7 @@ Main<Metavariables>::Main(CkArgMsg* msg) {
 
     if (parsed_command_line_options.count("check-options") != 0) {
       // Force all the options to be created.
-      options.template apply<option_list, Metavariables>([](auto... args) {
+      options.template apply<tmpl::list<>, Metavariables>([](auto... args) {
         (void)std::initializer_list<char>{((void)args, '0')...};
       });
       if (has_options) {
@@ -387,11 +387,11 @@ Main<Metavariables>::Main(CkArgMsg* msg) {
       sys::exit();
     }
 
-    options_ =
-        options.template apply<option_list, Metavariables>([](auto... args) {
-          return tuples::tagged_tuple_from_typelist<option_list>(
-              std::move(args)...);
-        });
+    // options_ =
+    //     options.template apply<option_list, Metavariables>([](auto... args) {
+    //       return tuples::tagged_tuple_from_typelist<option_list>(
+    //           std::move(args)...);
+    //     });
 
     // If any component specified that it needs resource information from
     // options, use the ResourceInfo created from options rather than the
