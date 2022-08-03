289d9f2c9b89da0061fbae29134c042cdb046ae1
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-08-03 04:08:41 -0700

    Make bodies of DistributedObject::simple_action() empty

--- src/Parallel/DistributedObject.hpp
+++ src/Parallel/DistributedObject.hpp
@@ -693,30 +693,30 @@ template <typename Action, typename... Args>
 void DistributedObject<ParallelComponent,
                        tmpl::list<PhaseDepActionListsPack...>>::
     simple_action(std::tuple<Args...> args) {
-  try {
-    (void)Parallel::charmxx::RegisterSimpleAction<ParallelComponent, Action,
-                                                  Args...>::registrar;
-    if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
-      node_lock_.lock();
-    }
-    if (performing_action_) {
-      ERROR(
-          "Already performing an Action and cannot execute additional Actions "
-          "from inside of an Action. This is only possible if the "
-          "simple_action function is not invoked via a proxy, which "
-          "we do not allow.");
-    }
-    performing_action_ = true;
-    forward_tuple_to_action<Action>(
-        std::move(args), std::make_index_sequence<sizeof...(Args)>{});
-    performing_action_ = false;
-    if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
-      node_lock_.unlock();
-    }
-    perform_algorithm();
-  } catch (const std::exception& exception) {
-    initiate_shutdown(exception);
-  }
+  // try {
+  //   (void)Parallel::charmxx::RegisterSimpleAction<ParallelComponent, Action,
+  //                                                 Args...>::registrar;
+  //   if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
+  //     node_lock_.lock();
+  //   }
+  //   if (performing_action_) {
+  //     ERROR(
+  //         "Already performing an Action and cannot execute additional Actions "
+  //         "from inside of an Action. This is only possible if the "
+  //         "simple_action function is not invoked via a proxy, which "
+  //         "we do not allow.");
+  //   }
+  //   performing_action_ = true;
+  //   forward_tuple_to_action<Action>(
+  //       std::move(args), std::make_index_sequence<sizeof...(Args)>{});
+  //   performing_action_ = false;
+  //   if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
+  //     node_lock_.unlock();
+  //   }
+  //   perform_algorithm();
+  // } catch (const std::exception& exception) {
+  //   initiate_shutdown(exception);
+  // }
 }
 
 template <typename ParallelComponent, typename... PhaseDepActionListsPack>
@@ -724,31 +724,31 @@ template <typename Action>
 void DistributedObject<
     ParallelComponent,
     tmpl::list<PhaseDepActionListsPack...>>::simple_action() {
-  try {
-    (void)Parallel::charmxx::RegisterSimpleAction<ParallelComponent,
-                                                  Action>::registrar;
-    if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
-      node_lock_.lock();
-    }
-    if (performing_action_) {
-      ERROR(
-          "Already performing an Action and cannot execute additional Actions "
-          "from inside of an Action. This is only possible if the "
-          "simple_action function is not invoked via a proxy, which "
-          "we do not allow.");
-    }
-    performing_action_ = true;
-    Action::template apply<ParallelComponent>(
-        box_, *Parallel::local_branch(global_cache_proxy_),
-        static_cast<const array_index&>(array_index_));
-    performing_action_ = false;
-    if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
-      node_lock_.unlock();
-    }
-    perform_algorithm();
-  } catch (const std::exception& exception) {
-    initiate_shutdown(exception);
-  }
+  // try {
+  //   (void)Parallel::charmxx::RegisterSimpleAction<ParallelComponent,
+  //                                                 Action>::registrar;
+  //   if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
+  //     node_lock_.lock();
+  //   }
+  //   if (performing_action_) {
+  //     ERROR(
+  //         "Already performing an Action and cannot execute additional Actions "
+  //         "from inside of an Action. This is only possible if the "
+  //         "simple_action function is not invoked via a proxy, which "
+  //         "we do not allow.");
+  //   }
+  //   performing_action_ = true;
+  //   Action::template apply<ParallelComponent>(
+  //       box_, *Parallel::local_branch(global_cache_proxy_),
+  //       static_cast<const array_index&>(array_index_));
+  //   performing_action_ = false;
+  //   if constexpr (std::is_same_v<Parallel::NodeLock, decltype(node_lock_)>) {
+  //     node_lock_.unlock();
+  //   }
+  //   perform_algorithm();
+  // } catch (const std::exception& exception) {
+  //   initiate_shutdown(exception);
+  // }
 }
 
 template <typename ParallelComponent, typename... PhaseDepActionListsPack>
