2f637c4e459098d0d0a1a8ce1c0b8993a4294366
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-08-04 19:57:51 -0700

    Simplify PARSE_ERROR and ERROR_NO_TRACE bodies

--- src/Parallel/Phase.cpp
+++ src/Parallel/Phase.cpp
@@ -81,8 +81,9 @@ Parallel::Phase Options::create_from_yaml<Parallel::Phase>::create<void>(
     }
   }
   using ::operator<<;
-  PARSE_ERROR(options.context(),
-              "Failed to convert \""
+  std::stringstream ss;
+  ss << "Failed to convert \""
                   << type_read << "\" to Parallel::Phase.\nMust be one of "
-                  << Parallel::known_phases() << ".");
+                  << Parallel::known_phases() << ".";
+  PARSE_ERROR(options.context(), ss);
 }
