ee8488af39c9ae3be802993dcf159a574ee244fd
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-08-09 02:18:23 -0700

    Also comment out the p | box_ call

--- src/Parallel/DistributedObject.hpp
+++ src/Parallel/DistributedObject.hpp
@@ -602,7 +602,7 @@ void DistributedObject<
   }
   // p | terminate_;
   // p | halt_algorithm_until_next_phase_;
-  p | box_;
+  // p | box_;
   // After unpacking the DataBox, we "touch" the GlobalCache proxy inside.
   // This forces the DataBox to recompute the GlobalCache* the next time it
   // is needed, but delays this process until after the pupper is called.
