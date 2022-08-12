5edd9bb96269c52c9dbbd9accbeea2f8554158c1
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2022-08-11 17:06:40 -0700

    Pass empty string to propagate_context in else of PARSE_ERROR

--- src/Options/Options.hpp
+++ src/Options/Options.hpp
@@ -77,8 +77,7 @@ inline std::ostream& operator<<(std::ostream& s, const Context& c) {
       std::ostringstream avoid_name_collisions_PARSE_ERROR;             \
       /* clang-tidy: macro arg in parentheses */                        \
       avoid_name_collisions_PARSE_ERROR << (context) << m; /* NOLINT */ \
-      throw ::Options::Options_detail::propagate_context(               \
-          avoid_name_collisions_PARSE_ERROR.str());                     \
+      throw ::Options::Options_detail::propagate_context("");           \
     }                                                                   \
   } while (false)
 
