5f80ad12a0c1772f891590e1dc012c3290652698
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-03-20 15:47:36 -0700

    Use round robin for BBH

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGhBinaryBlackHole.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGhBinaryBlackHole.hpp
@@ -181,6 +181,7 @@ struct EvolutionMetavars {
       dg::Formulation::StrongInertial;
   using temporal_id = Tags::TimeStepId;
   static constexpr bool local_time_stepping = true;
+  static constexpr bool use_z_order_distribution = false;
   // Set override_functions_of_time to true to override the
   // 2nd or 3rd order piecewise polynomial functions of time using
   // `read_spec_piecewise_polynomial()`
