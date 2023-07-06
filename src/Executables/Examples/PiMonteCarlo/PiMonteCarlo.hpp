81ded838f65859d9d591b5fa6582ca5df13e614b
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-07-06 11:14:55 -0700

    Add non-functional PiEstimator

--- src/Executables/Examples/PiMonteCarlo/PiMonteCarlo.hpp
+++ src/Executables/Examples/PiMonteCarlo/PiMonteCarlo.hpp
@@ -7,12 +7,45 @@
 #include <vector>
 
 #include "Options/String.hpp"
+#include "Parallel/Algorithms/AlgorithmSingleton.hpp"
+#include "Parallel/GlobalCache.hpp"
 #include "Parallel/Phase.hpp"
+#include "Parallel/PhaseDependentActionList.hpp"
 #include "Utilities/TMPL.hpp"
+
+// this isn't the parallel component itself, but rather specifies the
+// ingredients needed
+template <typename Metavars>
+struct PiEstimator {
+  using chare_type = Parallel::Algorithms::Singleton;
+  using metavariables = Metavars;
+  // type list of Actions that the code will do over and over in some phase
+  // until it hits some stop condition, where first arg to PhaseActions is the
+  // phase and second arg is the list of actions (tasks) to do
+  using phase_dependent_action_list = tmpl::list<
+      Parallel::PhaseActions<Parallel::Phase::Execute, tmpl::list<>>>;
+  using simple_tags_from_options = tmpl::list<>;
+  using const_global_cache_tags = tmpl::list<>;
+  using mutable_global_cache_tags = tmpl::list<>;
+
+  static void execute_next_phase(
+      const Parallel::Phase next_phase,
+      const Parallel::CProxy_GlobalCache<Metavars>& global_cache);
+};
+
+template <typename Metavars>
+void PiEstimator<Metavars>::execute_next_phase(
+    const Parallel::Phase next_phase,
+    const Parallel::CProxy_GlobalCache<Metavars>& global_cache) {
+  auto& local_cache = *Parallel::local_branch(global_cache);
+  // start_phase starts the phase dependent action list?
+  Parallel::get_parallel_component<PiEstimator<Metavars>>(local_cache)
+      .start_phase(next_phase);
+}
 
 struct Metavariables {
   // type list of parallel components
-  using component_list = tmpl::list<>;
+  using component_list = tmpl::list<PiEstimator<Metavariables>>;
 
   // phases of executable
   static constexpr std::array<Parallel::Phase, 3> default_phase_order{
@@ -22,6 +55,8 @@ struct Metavariables {
   // "about" the executable
   static constexpr Options::String help{
       "Compute pi via monte carlo integration"};
+
+  void pup(PUP::er& /*p*/) {}
 };
 
 // charm functions you want to turn on
