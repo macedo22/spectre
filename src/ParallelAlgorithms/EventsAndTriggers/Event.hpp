42f3491ef6eea1dfc1a01614bd9c69d5ea2834f6
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-09-24 01:37:56 -0700

    Revert "Also remove call to apply in Event::run()"
    This reverts commit 16562a9258561dc2f56042475eadbec206e0de82.

--- src/ParallelAlgorithms/EventsAndTriggers/Event.hpp
+++ src/ParallelAlgorithms/EventsAndTriggers/Event.hpp
@@ -50,16 +50,16 @@ class Event : public PUP::able {
   template <typename ComputeTagsList, typename DataBoxType,
             typename Metavariables, typename ArrayIndex,
             typename ComponentPointer>
-  void run(const ObservationBox<ComputeTagsList, DataBoxType>& /*box*/,
-           Parallel::GlobalCache<Metavariables>& /*cache*/,
-           const ArrayIndex& /*array_index*/, const ComponentPointer /*meta*/,
-           const ObservationValue& /*observation_value*/) const {
+  void run(const ObservationBox<ComputeTagsList, DataBoxType>& box,
+           Parallel::GlobalCache<Metavariables>& cache,
+           const ArrayIndex& array_index, const ComponentPointer /*meta*/,
+           const ObservationValue& observation_value) const {
     using factory_classes =
         typename std::decay_t<Metavariables>::factory_creation::factory_classes;
     call_with_dynamic_type<void, tmpl::at<factory_classes, Event>>(
-        this, [&](auto* const /*event*/) {
-          // apply(*event, box, cache, array_index, ComponentPointer{},
-          //       observation_value);
+        this, [&](auto* const event) {
+          apply(*event, box, cache, array_index, ComponentPointer{},
+                observation_value);
         });
   }
 
