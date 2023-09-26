7325a5de24e73a44e182ec887fb82d5a921ecc31
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-09-26 15:47:26 -0700

    Revert "Remove else body of print_impl<Tag, OptionList>"
    This reverts commit 28b790ea73c1387d77d984d55ed39de3990b85b1.

--- src/Options/OptionsDetails.hpp
+++ src/Options/OptionsDetails.hpp
@@ -236,11 +236,10 @@ struct print_impl {
       ss << "\n" << wrap_text(Tag::help, 77, indent + "  ") << "\n\n";
       return ss.str();
     } else {
-      (void)indent;
       // A group
       std::ostringstream ss;
-      // ss << indent << pretty_type::name<Tag>() << ":\n"
-      //    << wrap_text(Tag::help, 77, indent + "  ") << "\n\n";
+      ss << indent << pretty_type::name<Tag>() << ":\n"
+         << wrap_text(Tag::help, 77, indent + "  ") << "\n\n";
       return ss.str();
     }
   }
