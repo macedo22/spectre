68cdd7a9c3e391bef88d69ddb2b4179c4d5b6ccb
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-09-26 14:46:47 -0700

    Revert "Remove ss << from if body of print_impl except for the one in the call_with_dynamic_type lambda"
    This reverts commit 84cb775a03b02ff96d2f2c8ec40595a4733746f5.

--- src/Options/OptionsDetails.hpp
+++ src/Options/OptionsDetails.hpp
@@ -205,8 +205,8 @@ struct print_impl {
     if constexpr (tmpl::list_contains_v<OptionList, Tag>) {
       const std::string new_line = "\n" + indent + "  ";
       std::ostringstream ss;
-      // ss << indent << pretty_type::name<Tag>() << ":" << new_line
-      //    << "type=" << yaml_type<typename Tag::type>::value();
+      ss << indent << pretty_type::name<Tag>() << ":" << new_line
+         << "type=" << yaml_type<typename Tag::type>::value();
       if constexpr (has_suggested<Tag>::value) {
         if constexpr (tt::is_a_v<std::unique_ptr, typename Tag::type>) {
           call_with_dynamic_type<
@@ -217,23 +217,23 @@ struct print_impl {
                    << pretty_type::short_name<decltype(*derived)>();
               });
         } else {
-          // ss << new_line << "suggested="
-          //    << (MakeString{} << std::boolalpha << Tag::suggested_value());
+          ss << new_line << "suggested="
+             << (MakeString{} << std::boolalpha << Tag::suggested_value());
         }
       }
       if constexpr (has_lower_bound<Tag>::value) {
-        // ss << new_line << "min=" << (MakeString{} << Tag::lower_bound());
+        ss << new_line << "min=" << (MakeString{} << Tag::lower_bound());
       }
       if constexpr (has_upper_bound<Tag>::value) {
-        // ss << new_line << "max=" << (MakeString{} << Tag::upper_bound());
+        ss << new_line << "max=" << (MakeString{} << Tag::upper_bound());
       }
       if constexpr (has_lower_bound_on_size<Tag>::value) {
-        // ss << new_line << "min size=" << Tag::lower_bound_on_size();
+        ss << new_line << "min size=" << Tag::lower_bound_on_size();
       }
       if constexpr (has_upper_bound_on_size<Tag>::value) {
-        // ss << new_line << "max size=" << Tag::upper_bound_on_size();
+        ss << new_line << "max size=" << Tag::upper_bound_on_size();
       }
-      // ss << "\n" << wrap_text(Tag::help, 77, indent + "  ") << "\n\n";
+      ss << "\n" << wrap_text(Tag::help, 77, indent + "  ") << "\n\n";
       return ss.str();
     } else {
       // A group
