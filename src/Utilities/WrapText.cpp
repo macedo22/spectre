43814f6af8604debd0c229eee9ab9f3064ea120c
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-09-28 22:57:52 -0700

    Use alternative wrap_text in print_impl that takes second string arg by value and no const

--- src/Utilities/WrapText.cpp
+++ src/Utilities/WrapText.cpp
@@ -51,3 +51,8 @@ std::string wrap_text(std::string str, const size_t line_length,
   }
   return str;
 }
+
+std::string wrap_text_(std::string /*str*/, size_t /*line_length*/,
+                       const std::string /*indentation*/) {
+  return "";
+}
