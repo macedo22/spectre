a6962e58a1e3283f2bdb8ca468857f6cbb04f43f
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-10-13 16:10:38 -0700

    Revert "Move call to wrapped_text in print_impl outside of ss << expression"
    This reverts commit 90e1113991e3636751fb833686100552c6de259e.

--- src/Options/OptionsDetails.hpp
+++ src/Options/OptionsDetails.hpp
@@ -233,8 +233,7 @@ struct print_impl {
       if constexpr (has_upper_bound_on_size<Tag>::value) {
         ss << new_line << "max size=" << Tag::upper_bound_on_size();
       }
-      const std::string wrapped_text = wrap_text(Tag::help, 77, indent + "  ");
-      ss << "\n" << wrapped_text << "\n\n";
+      ss << "\n" << wrap_text(Tag::help, 77, indent + "  ") << "\n\n";
       return ss.str();
     } else {
       // A group
