5ed677ef0baf006fefe26b961396d89a85f226bd
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-10-19 06:49:35 -0700

    Revert "Remove bodies of register_classes_with_charm"
    This reverts commit eccfb880d1e772aaab87e8ffc5bd279cd17e1249.

--- src/Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp
+++ src/Utilities/Serialization/RegisterDerivedClassesWithCharm.hpp
@@ -59,30 +59,30 @@ std::string registration_name() {
 template <typename... Registrants>
 void register_classes_with_charm(
     const tmpl::list<Registrants...> /*meta*/ = {}) {
-  // const auto helper = [](auto class_v) {
-  //   using class_to_register = typename decltype(class_v)::type;
-  //   // We use PUPable_reg2 because this takes as a second argument the name of
-  //   // the class (as a `const char*`), while PUPable_reg converts the argument
-  //   // verbatim to a string using the `#` preprocessor operator.
-  //   PUPable_reg2(class_to_register,
-  //                registration_name<class_to_register>().c_str());
-  // };
-  // (void)helper;
-  // EXPAND_PACK_LEFT_TO_RIGHT(helper(tmpl::type_<Registrants>{}));
+  const auto helper = [](auto class_v) {
+    using class_to_register = typename decltype(class_v)::type;
+    // We use PUPable_reg2 because this takes as a second argument the name of
+    // the class (as a `const char*`), while PUPable_reg converts the argument
+    // verbatim to a string using the `#` preprocessor operator.
+    PUPable_reg2(class_to_register,
+                 registration_name<class_to_register>().c_str());
+  };
+  (void)helper;
+  EXPAND_PACK_LEFT_TO_RIGHT(helper(tmpl::type_<Registrants>{}));
 }
 
 /// Register derived classes of the `Base` class
 template <typename Base>
 void register_derived_classes_with_charm() {
-  // register_classes_with_charm(typename Base::creatable_classes{});
+  register_classes_with_charm(typename Base::creatable_classes{});
 }
 
 /// Register all classes in Metavariables::factory_classes
 template <typename Metavariables>
 void register_factory_classes_with_charm() {
-  // register_classes_with_charm(
-  //     tmpl::filter<
-  //         tmpl::flatten<tmpl::values_as_sequence<
-  //             typename Metavariables::factory_creation::factory_classes>>,
-  //         std::is_base_of<PUP::able, tmpl::_1>>{});
+  register_classes_with_charm(
+      tmpl::filter<
+          tmpl::flatten<tmpl::values_as_sequence<
+              typename Metavariables::factory_creation::factory_classes>>,
+          std::is_base_of<PUP::able, tmpl::_1>>{});
 }
