4aebab3e06ab910249c8a37bcbe5d430dd5b1bb4
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-10-19 19:36:34 -0700

    Remove all of interpolate body except for Variables creation

--- src/ParallelAlgorithms/Interpolation/Interpolate.hpp
+++ src/ParallelAlgorithms/Interpolation/Interpolate.hpp
@@ -32,37 +32,38 @@ namespace intrp {
 template <typename InterpolationTargetTag, size_t VolumeDim,
           typename Metavariables, typename... InterpolatorSourceVars>
 void interpolate(
-    const typename InterpolationTargetTag::temporal_id::type& temporal_id,
-    const Mesh<VolumeDim>& mesh, Parallel::GlobalCache<Metavariables>& cache,
-    const ElementId<VolumeDim>& array_index,
-    const InterpolatorSourceVars&... interpolator_source_vars_input) {
+    const typename InterpolationTargetTag::temporal_id::type& /*temporal_id*/,
+    const Mesh<VolumeDim>& mesh, Parallel::GlobalCache<Metavariables>& /*cache*/,
+    const ElementId<VolumeDim>& /*array_index*/,
+    const InterpolatorSourceVars&... /*interpolator_source_vars_input*/) {
   Variables<typename Metavariables::interpolator_source_vars>
       interpolator_source_vars(mesh.number_of_grid_points());
-  const std::tuple<const InterpolatorSourceVars&...>
-      interpolator_source_vars_tuple{interpolator_source_vars_input...};
-  tmpl::for_each<
-      tmpl::make_sequence<tmpl::size_t<0>, sizeof...(InterpolatorSourceVars)>>(
-      [&interpolator_source_vars,
-       &interpolator_source_vars_tuple](auto index_v) {
-        constexpr size_t index = tmpl::type_from<decltype(index_v)>::value;
-        get<tmpl::at_c<typename Metavariables::interpolator_source_vars,
-                       index>>(interpolator_source_vars) =
-            get<index>(interpolator_source_vars_tuple);
-      });
+  (void)interpolator_source_vars;
+//   const std::tuple<const InterpolatorSourceVars&...>
+//       interpolator_source_vars_tuple{interpolator_source_vars_input...};
+//   tmpl::for_each<
+//       tmpl::make_sequence<tmpl::size_t<0>, sizeof...(InterpolatorSourceVars)>>(
+//       [&interpolator_source_vars,
+//        &interpolator_source_vars_tuple](auto index_v) {
+//         constexpr size_t index = tmpl::type_from<decltype(index_v)>::value;
+//         get<tmpl::at_c<typename Metavariables::interpolator_source_vars,
+//                        index>>(interpolator_source_vars) =
+//             get<index>(interpolator_source_vars_tuple);
+//       });
 
-  // Send volume data to the Interpolator, to trigger interpolation.
-  auto& interpolator = *Parallel::local_branch(
-      ::Parallel::get_parallel_component<Interpolator<Metavariables>>(cache));
-  Parallel::simple_action<Actions::InterpolatorReceiveVolumeData<
-      typename InterpolationTargetTag::temporal_id>>(
-      interpolator, temporal_id, array_index, mesh, interpolator_source_vars);
+//   // Send volume data to the Interpolator, to trigger interpolation.
+//   auto& interpolator = *Parallel::local_branch(
+//       ::Parallel::get_parallel_component<Interpolator<Metavariables>>(cache));
+//   Parallel::simple_action<Actions::InterpolatorReceiveVolumeData<
+//       typename InterpolationTargetTag::temporal_id>>(
+//       interpolator, temporal_id, array_index, mesh, interpolator_source_vars);
 
-  // Tell the interpolation target that it should interpolate.
-  auto& target = Parallel::get_parallel_component<
-      InterpolationTarget<Metavariables, InterpolationTargetTag>>(cache);
-  Parallel::simple_action<
-      Actions::AddTemporalIdsToInterpolationTarget<InterpolationTargetTag>>(
-      target, std::vector<typename InterpolationTargetTag::temporal_id::type>{
-                  temporal_id});
+//   // Tell the interpolation target that it should interpolate.
+//   auto& target = Parallel::get_parallel_component<
+//       InterpolationTarget<Metavariables, InterpolationTargetTag>>(cache);
+//   Parallel::simple_action<
+//       Actions::AddTemporalIdsToInterpolationTarget<InterpolationTargetTag>>(
+//       target, std::vector<typename InterpolationTargetTag::temporal_id::type>{
+//                   temporal_id});
 }
 }  // namespace intrp
