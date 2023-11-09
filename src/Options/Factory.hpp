c58c56c7ed159286e1346db4e857ea7a453d910b
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-11-09 01:45:09 -0800

    Use empty body for Options::Factor_detail::help_derived

--- src/Options/Factory.hpp
+++ src/Options/Factory.hpp
@@ -57,10 +57,7 @@ struct print_derived {
 
 template <typename CreatableClasses>
 std::string help_derived() {
-  return "Known Ids:\n" +
-         tmpl::for_each<CreatableClasses>(
-             Factory_detail::print_derived{})
-             .value;
+  return "";
 }
 
 // This is for handling legacy code that still uses creatable_classes.
