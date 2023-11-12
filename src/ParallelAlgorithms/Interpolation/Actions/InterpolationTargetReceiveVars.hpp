13044b8211100ecf5557f8892f70efb80e462d7b
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-11-11 17:54:22 -0800

    Remove call to CleanUpInterpolator in InterpolationTargetReceiveVars

--- src/ParallelAlgorithms/Interpolation/Actions/InterpolationTargetReceiveVars.hpp
+++ src/ParallelAlgorithms/Interpolation/Actions/InterpolationTargetReceiveVars.hpp
@@ -136,9 +136,10 @@ struct InterpolationTargetReceiveVars {
         auto& interpolator_proxy =
             Parallel::get_parallel_component<Interpolator<Metavariables>>(
                 cache);
-        Parallel::simple_action<
-            Actions::CleanUpInterpolator<InterpolationTargetTag>>(
-            interpolator_proxy, temporal_id);
+        (void)interpolator_proxy;
+        // Parallel::simple_action<
+        //     Actions::CleanUpInterpolator<InterpolationTargetTag>>(
+        //     interpolator_proxy, temporal_id);
 
         // If we have a sequential target, and there are further
         // temporal_ids, begin interpolation for the next one.
