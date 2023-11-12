c6999859c3d2a4a7faabc8a6f4525291108ea798
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-11-11 19:03:41 -0800

    Remove body of ObserveSurfaceData::apply

--- src/ParallelAlgorithms/Interpolation/Callbacks/ObserveSurfaceData.hpp
+++ src/ParallelAlgorithms/Interpolation/Callbacks/ObserveSurfaceData.hpp
@@ -58,71 +58,71 @@ struct ObserveSurfaceData
   using const_global_cache_tags = tmpl::list<observers::Tags::SurfaceFileName>;
 
   template <typename DbTags, typename Metavariables, typename TemporalId>
-  static void apply(const db::DataBox<DbTags>& box,
-                    Parallel::GlobalCache<Metavariables>& cache,
-                    const TemporalId& temporal_id) {
-    const auto& strahlkorper = get<ylm::Tags::Strahlkorper<HorizonFrame>>(box);
-    const ylm::Spherepack& ylm = strahlkorper.ylm_spherepack();
+  static void apply(const db::DataBox<DbTags>& /*box*/,
+                    Parallel::GlobalCache<Metavariables>& /*cache*/,
+                    const TemporalId& /*temporal_id*/) {
+    // const auto& strahlkorper = get<ylm::Tags::Strahlkorper<HorizonFrame>>(box);
+    // const ylm::Spherepack& ylm = strahlkorper.ylm_spherepack();
 
-    // Output the inertial-frame coordinates of the Stralhlkorper.
-    // Note that these coordinates are not
-    // Spherepack-evenly-distributed over the inertial-frame sphere
-    // (they are Spherepack-evenly-distributed over the HorizonFrame
-    // sphere).
-    std::vector<TensorComponent> tensor_components;
-    if constexpr (db::tag_is_retrievable_v<
-                      ylm::Tags::CartesianCoords<::Frame::Inertial>,
-                      db::DataBox<DbTags>>) {
-      const auto& inertial_strahlkorper_coords =
-          get<ylm::Tags::CartesianCoords<::Frame::Inertial>>(box);
-      tensor_components.push_back(
-          {"InertialCoordinates_x"s, get<0>(inertial_strahlkorper_coords)});
-      tensor_components.push_back(
-          {"InertialCoordinates_y"s, get<1>(inertial_strahlkorper_coords)});
-      tensor_components.push_back(
-          {"InertialCoordinates_z"s, get<2>(inertial_strahlkorper_coords)});
-    }
-    // Output each tag if it is a scalar. Otherwise, throw a compile-time
-    // error. This could be generalized to handle tensors of nonzero rank by
-    // looping over the components, so each component could be visualized
-    // separately as a scalar. But in practice, this generalization is
-    // probably unnecessary, because Strahlkorpers are typically only
-    // visualized with scalar quantities (used set the color at different
-    // points on the surface).
-    tmpl::for_each<TagsToObserve>([&box, &tensor_components](auto tag_v) {
-      using Tag = tmpl::type_from<decltype(tag_v)>;
-      const auto tag_name = db::tag_name<Tag>();
-      const auto& tensor = get<Tag>(box);
-      for (size_t i = 0; i < tensor.size(); ++i) {
-        tensor_components.emplace_back(tag_name + tensor.component_suffix(i),
-                                       tensor[i]);
-      }
-    });
+    // // Output the inertial-frame coordinates of the Stralhlkorper.
+    // // Note that these coordinates are not
+    // // Spherepack-evenly-distributed over the inertial-frame sphere
+    // // (they are Spherepack-evenly-distributed over the HorizonFrame
+    // // sphere).
+    // std::vector<TensorComponent> tensor_components;
+    // if constexpr (db::tag_is_retrievable_v<
+    //                   ylm::Tags::CartesianCoords<::Frame::Inertial>,
+    //                   db::DataBox<DbTags>>) {
+    //   const auto& inertial_strahlkorper_coords =
+    //       get<ylm::Tags::CartesianCoords<::Frame::Inertial>>(box);
+    //   tensor_components.push_back(
+    //       {"InertialCoordinates_x"s, get<0>(inertial_strahlkorper_coords)});
+    //   tensor_components.push_back(
+    //       {"InertialCoordinates_y"s, get<1>(inertial_strahlkorper_coords)});
+    //   tensor_components.push_back(
+    //       {"InertialCoordinates_z"s, get<2>(inertial_strahlkorper_coords)});
+    // }
+    // // Output each tag if it is a scalar. Otherwise, throw a compile-time
+    // // error. This could be generalized to handle tensors of nonzero rank by
+    // // looping over the components, so each component could be visualized
+    // // separately as a scalar. But in practice, this generalization is
+    // // probably unnecessary, because Strahlkorpers are typically only
+    // // visualized with scalar quantities (used set the color at different
+    // // points on the surface).
+    // tmpl::for_each<TagsToObserve>([&box, &tensor_components](auto tag_v) {
+    //   using Tag = tmpl::type_from<decltype(tag_v)>;
+    //   const auto tag_name = db::tag_name<Tag>();
+    //   const auto& tensor = get<Tag>(box);
+    //   for (size_t i = 0; i < tensor.size(); ++i) {
+    //     tensor_components.emplace_back(tag_name + tensor.component_suffix(i),
+    //                                    tensor[i]);
+    //   }
+    // });
 
-    const std::string& surface_name =
-        pretty_type::name<InterpolationTargetTag>();
-    const std::string subfile_path{std::string{"/"} + surface_name};
-    const std::vector<size_t> extents_vector{
-        {ylm.physical_extents()[0], ylm.physical_extents()[1]}};
-    const std::vector<Spectral::Basis> bases_vector{
-        2, Spectral::Basis::SphericalHarmonic};
-    const std::vector<Spectral::Quadrature> quadratures_vector{
-        {Spectral::Quadrature::Gauss, Spectral::Quadrature::Equiangular}};
-    const observers::ObservationId& observation_id = observers::ObservationId(
-        InterpolationTarget_detail::get_temporal_id_value(temporal_id),
-        subfile_path + ".vol");
+    // const std::string& surface_name =
+    //     pretty_type::name<InterpolationTargetTag>();
+    // const std::string subfile_path{std::string{"/"} + surface_name};
+    // const std::vector<size_t> extents_vector{
+    //     {ylm.physical_extents()[0], ylm.physical_extents()[1]}};
+    // const std::vector<Spectral::Basis> bases_vector{
+    //     2, Spectral::Basis::SphericalHarmonic};
+    // const std::vector<Spectral::Quadrature> quadratures_vector{
+    //     {Spectral::Quadrature::Gauss, Spectral::Quadrature::Equiangular}};
+    // const observers::ObservationId& observation_id = observers::ObservationId(
+    //     InterpolationTarget_detail::get_temporal_id_value(temporal_id),
+    //     subfile_path + ".vol");
 
-    auto& proxy = Parallel::get_parallel_component<
-        observers::ObserverWriter<Metavariables>>(cache);
+    // auto& proxy = Parallel::get_parallel_component<
+    //     observers::ObserverWriter<Metavariables>>(cache);
 
-    // We call this on proxy[0] because the 0th element of a NodeGroup is
-    // always guaranteed to be present.
-    Parallel::threaded_action<observers::ThreadedActions::WriteVolumeData>(
-        proxy[0], Parallel::get<observers::Tags::SurfaceFileName>(cache),
-        subfile_path, observation_id,
-        std::vector<ElementVolumeData>{{surface_name, tensor_components,
-                                        extents_vector, bases_vector,
-                                        quadratures_vector}});
+    // // We call this on proxy[0] because the 0th element of a NodeGroup is
+    // // always guaranteed to be present.
+    // Parallel::threaded_action<observers::ThreadedActions::WriteVolumeData>(
+    //     proxy[0], Parallel::get<observers::Tags::SurfaceFileName>(cache),
+    //     subfile_path, observation_id,
+    //     std::vector<ElementVolumeData>{{surface_name, tensor_components,
+    //                                     extents_vector, bases_vector,
+    //                                     quadratures_vector}});
   }
 };
 }  // namespace callbacks
