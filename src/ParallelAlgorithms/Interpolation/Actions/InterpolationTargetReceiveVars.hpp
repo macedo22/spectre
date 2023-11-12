46050bfb620eec6ecc66ed0343c3ffe04172a7e3
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-11-11 18:19:25 -0800

    Revert "Remove call to call_callback in InterpolationTargetReceiveVars::apply"
    This reverts commit ed8751a07352a29e444013e2449dba300ef30157.

--- src/ParallelAlgorithms/Interpolation/Actions/InterpolationTargetReceiveVars.hpp
+++ src/ParallelAlgorithms/Interpolation/Actions/InterpolationTargetReceiveVars.hpp
@@ -129,7 +129,8 @@ struct InterpolationTargetReceiveVars {
     if (InterpolationTarget_detail::have_data_at_all_points<
             InterpolationTargetTag>(box, temporal_id)) {
       // All the valid points have been interpolated.
-      if (true) {
+      if (InterpolationTarget_detail::call_callback<InterpolationTargetTag>(
+              make_not_null(&box), make_not_null(&cache), temporal_id)) {
         InterpolationTarget_detail::clean_up_interpolation_target<
             InterpolationTargetTag>(make_not_null(&box), temporal_id);
         auto& interpolator_proxy =
