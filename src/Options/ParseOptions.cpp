6ce5e530bab39660bfaf3c74aaff24b68451216f
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2023-12-02 18:55:47 -0800

    Factor out parts of body of parse

--- src/Options/ParseOptions.cpp
+++ src/Options/ParseOptions.cpp
@@ -3,13 +3,23 @@
 
 #include "Options/ParseOptions.hpp"
 
+#include <cstddef>
 #include <exception>
+#include <limits>
+#include <ostream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 #include <yaml-cpp/yaml.h>
 
 #include "Informer/InfoFromBuild.hpp"
+#include "Options/Context.hpp"
+#include "Options/ParseError.hpp"
+#include "Parallel/Printf.hpp"
+#include "Utilities/Algorithm.hpp"
 #include "Utilities/FileSystem.hpp"
+#include "Utilities/Gsl.hpp"
+#include "Utilities/MakeString.hpp"
 
 namespace Options::detail {
 namespace {
@@ -61,6 +71,84 @@ YAML::Node load_and_check_yaml(const std::string& options,
   } else {
     throw std::runtime_error("Expected either one or two YAML documents, not " +
                              std::to_string(yaml_docs.size()) + ".");
+  }
+}
+
+std::unordered_set<std::string> get_given_options(
+    const Options::Context& context, const YAML::Node& node,
+    const std::string& help) {
+  if (not(node.IsMap() or node.IsNull())) {
+    PARSE_ERROR(context, "'" << node << "' does not look like options.\n"
+                             << help);
+  }
+
+  std::unordered_set<std::string> given_options{};
+  for (const auto& name_and_value : node) {
+    given_options.insert(name_and_value.first.as<std::string>());
+  }
+  return given_options;
+}
+
+void check_for_unique_choice(const std::vector<size_t>& alternative_choices,
+                             const Options::Context& context,
+                             const std::string& parsing_help) {
+  if (alg::any_of(alternative_choices, [](const size_t x) {
+        return x == std::numeric_limits<size_t>::max();
+      })) {
+    PARSE_ERROR(context, "Cannot decide between alternative options.\n"
+                             << parsing_help);
+  }
+}
+
+void check_for_duplicate_option_name(
+    const gsl::not_null<std::vector<std::string>*> result,
+    const std::string& label) {
+  ASSERT(alg::find(*result, label) == result->end(),
+         "Duplicate option name: " << label);
+  result->push_back(label);
+}
+
+[[noreturn]] void option_specified_twice_error(
+    const Options::Context& context, const std::string& name,
+    const std::string& parsing_help) {
+  PARSE_ERROR(context, "Option '" << name << "' specified twice.\n"
+                                  << parsing_help);
+}
+
+[[noreturn]] void unused_key_error(const Context& context,
+                                   const std::string& name,
+                                   const std::string& parsing_help) {
+  PARSE_ERROR(context, "Option '"
+                           << name
+                           << "' is unused because of other provided options.\n"
+                           << parsing_help);
+}
+
+[[noreturn]] void option_invalid_error(const Options::Context& context,
+                                       const std::string& name,
+                                       const std::string& parsing_help) {
+  PARSE_ERROR(context, "Option '" << name << "' is not a valid option.\n"
+                                  << parsing_help);
+}
+
+[[noreturn]] void missing_option_error(
+    const Options::Context& context,
+    const std::vector<std::string>& valid_names,
+    const std::string& parsing_help) {
+  PARSE_ERROR(context, "You did not specify the option"
+                           << (valid_names.size() == 1 ? " " : "s ")
+                           << (MakeString{} << valid_names) << "\n"
+                           << parsing_help);
+}
+
+std::string add_group_prefix_to_name(const std::string& name) {
+  return "In group " + name;
+}
+
+void print_top_level_error_message_if_top_level(bool is_top_level) {
+  if (is_top_level) {
+    Parallel::printf_error(
+        "The following options differ from their suggested values:\n");
   }
 }
 }  // namespace Options::detail
