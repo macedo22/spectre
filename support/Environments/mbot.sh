693f721f640a53b3b1afe265117c2a15f926ad45
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2024-08-30 19:34:28 -0400

    Turn on address sanitizer

--- support/Environments/mbot.sh
+++ support/Environments/mbot.sh
@@ -36,6 +36,7 @@ spectre_run_cmake_gcc() {
           -D MACHINE=Mbot \
           -D USE_XSIMD=yes \
           -D DEBUG_SYMBOLS=ON \
+          -D ASAN=ON \
           "$@" \
           $SPECTRE_HOME
 }
