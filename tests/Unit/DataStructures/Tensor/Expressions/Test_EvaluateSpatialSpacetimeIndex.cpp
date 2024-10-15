7ed0fe9f26080677d6857b2b5c4a7cd9b3ec439d
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2024-10-15 10:37:55 -0700

    Remove unneeded rhs test for spatial spacetime indices

--- tests/Unit/DataStructures/Tensor/Expressions/Test_EvaluateSpatialSpacetimeIndex.cpp
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_EvaluateSpatialSpacetimeIndex.cpp
@@ -69,11 +69,10 @@ void test_rhs() {
       index_list<SpatialIndex<3, UpLo::Lo, Frame::Inertial>,
                  SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>>>();
 
-  TestHelpers::tenex::test_evaluate_rank_2_core<
-      false, ti::J, ti::I, DataType, Symmetry<1, 1>,
+  TestHelpers::tenex::test_evaluate_rank_2_impl<
+      true, ti::J, ti::I, DataType, Symmetry<1, 1>,
       index_list<SpacetimeIndex<3, UpLo::Up, Frame::Distorted>,
                  SpacetimeIndex<3, UpLo::Up, Frame::Distorted>>,
-      Symmetry<2, 1>,
       index_list<SpatialIndex<3, UpLo::Up, Frame::Distorted>,
                  SpatialIndex<3, UpLo::Up, Frame::Distorted>>>();
 
@@ -83,20 +82,6 @@ void test_rhs() {
                  SpacetimeIndex<3, UpLo::Lo, Frame::Grid>>,
       index_list<SpatialIndex<3, UpLo::Lo, Frame::Grid>,
                  SpatialIndex<3, UpLo::Lo, Frame::Grid>>>();
-
-  TestHelpers::tenex::test_evaluate_rank_2_impl<
-      false, ti::K, ti::L, DataType, Symmetry<1, 1>,
-      index_list<SpacetimeIndex<3, UpLo::Up, Frame::Inertial>,
-                 SpacetimeIndex<3, UpLo::Up, Frame::Inertial>>,
-      index_list<SpatialIndex<3, UpLo::Up, Frame::Inertial>,
-                 SpatialIndex<3, UpLo::Up, Frame::Inertial>>>();
-
-  TestHelpers::tenex::test_evaluate_rank_2_impl<
-      true, ti::m, ti::j, DataType, Symmetry<1, 1>,
-      index_list<SpacetimeIndex<2, UpLo::Lo, Frame::Grid>,
-                 SpacetimeIndex<2, UpLo::Lo, Frame::Grid>>,
-      index_list<SpatialIndex<2, UpLo::Lo, Frame::Grid>,
-                 SpatialIndex<2, UpLo::Lo, Frame::Grid>>>();
 }
 
 // \brief Test evaluation of tensors where generic spatial indices are used for
