041bcb5534630fbebe6ef9e65c5a8825ecfb32bf
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2024-10-15 10:51:04 -0700

    Add another rank 4 tests for spatial spacetime indices

--- tests/Unit/DataStructures/Tensor/Expressions/Test_EvaluateSpatialSpacetimeIndex.cpp
+++ tests/Unit/DataStructures/Tensor/Expressions/Test_EvaluateSpatialSpacetimeIndex.cpp
@@ -274,17 +274,31 @@ void test_rhs_and_lhs_rank2() {
 // \tparam DataType the type of data being stored in the expression operands
 template <typename DataType>
 void test_rhs_and_lhs_rank4() {
+  // tests that return type is what is expected
   TestHelpers::tenex::test_evaluate_rank_4_core<
-      true, ti::i, ti::k, ti::a, ti::j, DataType, Symmetry<3, 2, 2, 1>,
+      true, ti::i, ti::k, ti::a, ti::j, DataType, Symmetry<1, 2, 2, 1>,
       index_list<SpatialIndex<3, UpLo::Lo, Frame::Grid>,
-                 SpacetimeIndex<2, UpLo::Lo, Frame::Grid>,
-                 SpacetimeIndex<2, UpLo::Lo, Frame::Grid>,
-                 SpatialIndex<2, UpLo::Lo, Frame::Inertial>>,
-      Symmetry<4, 3, 2, 1>,
+                 SpacetimeIndex<2, UpLo::Lo, Frame::Inertial>,
+                 SpacetimeIndex<2, UpLo::Lo, Frame::Inertial>,
+                 SpatialIndex<3, UpLo::Lo, Frame::Grid>>,
+      Symmetry<1, 3, 2, 1>,
       index_list<SpatialIndex<3, UpLo::Lo, Frame::Grid>,
-                 SpatialIndex<2, UpLo::Lo, Frame::Grid>,
-                 SpacetimeIndex<2, UpLo::Lo, Frame::Grid>,
-                 SpatialIndex<2, UpLo::Lo, Frame::Inertial>>>();
+                 SpatialIndex<2, UpLo::Lo, Frame::Inertial>,
+                 SpacetimeIndex<2, UpLo::Lo, Frame::Inertial>,
+                 SpatialIndex<3, UpLo::Lo, Frame::Grid>>>();
+
+  // tests that only spatial components are filled for LHS tensor arg
+  TestHelpers::tenex::test_evaluate_rank_4_core<
+      false, ti::m, ti::c, ti::i, ti::j, DataType, Symmetry<1, 1, 2, 1>,
+      index_list<SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>,
+                 SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>,
+                 SpatialIndex<3, UpLo::Lo, Frame::Inertial>,
+                 SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>>,
+      Symmetry<1, 1, 2, 1>,
+      index_list<SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>,
+                 SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>,
+                 SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>,
+                 SpacetimeIndex<3, UpLo::Lo, Frame::Inertial>>>();
 }
 
 template <typename DataType>
