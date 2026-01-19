f974fdff0ae76dd431e61d4b062d8e2189eeb3ca
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2026-01-19 12:11:55 -0800

    Remove pragma once in EvaluateRankN.cpp

--- tests/Unit/Helpers/DataStructures/Tensor/Expressions/EvaluateRankN.cpp
+++ tests/Unit/Helpers/DataStructures/Tensor/Expressions/EvaluateRankN.cpp
@@ -1,9 +1,8 @@
 // Distributed under the MIT License.
 // See LICENSE.txt for details.
 
-#pragma once
-
 #include "Helpers/DataStructures/Tensor/Expressions/EvaluateRankN.hpp"
+
 #include "DataStructures/DataVector.hpp"
 #include "Helpers/DataStructures/Tensor/Expressions/EvaluateRank0.hpp"
 
