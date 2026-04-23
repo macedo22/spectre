06081ce4a95dd9e6f14befb5ebda572f73159445
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2026-04-23 14:38:29 -0400

    Revert "Attempt to fix the fix with adding new map to maps_list"
    This reverts commit 7cec40faa883f3cf393860cde76449a814bc3a99.

--- src/Domain/Creators/CylindricalBinaryCompactObject.hpp
+++ src/Domain/Creators/CylindricalBinaryCompactObject.hpp
@@ -35,7 +35,6 @@ template <typename Map1, typename Map2>
 class ProductOf2Maps;
 template <typename Map1, typename Map2, typename Map3>
 class ProductOf3Maps;
-class SphericalToCartesianPfaffian;
 template <size_t VolumeDim>
 class Wedge;
 template <size_t VolumeDim>
@@ -186,12 +185,6 @@ class CylindricalBinaryCompactObject : public DomainCreator<3> {
                                                     CoordinateMaps::Interval>,
                      CoordinateMaps::UniformCylindricalSide,
                      CoordinateMaps::DiscreteRotation<3>>,
-                 domain::CoordinateMap<
-                     Frame::BlockLogical, Frame::Inertial,
-                     domain::CoordinateMaps::ProductOf3Maps<
-                         CoordinateMaps::Interval, CoordinateMaps::Interval,
-                         CoordinateMaps::Interval>,
-                     domain::CoordinateMaps::SphericalToCartesianPfaffian>,
                  bco::TimeDependentMapOptions<true>::maps_list>>;
 
   struct CenterA {
