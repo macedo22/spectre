60bf88c4ca017a652685dc8ef25d32dd55ecccb5
Alexandra Macedo <alexandra.l.macedo@gmail.com>
2026-06-08 14:55:23 -0700

    Fix input string in CBCO test

--- tests/Unit/Domain/Creators/Test_CylindricalBinaryCompactObject.cpp
+++ tests/Unit/Domain/Creators/Test_CylindricalBinaryCompactObject.cpp
@@ -261,11 +261,13 @@ std::string create_option_string(
                              "      CoefficientTruncationLimit: 0.\n"
                              "      InitialValues: Spherical\n"
                              "      SizeInitialValues: [1.1, 0.0, 0.0]\n"
+                             "      TransitionEndsAtCube: false\n"
                              "    ShapeMapB:\n"
                              "      LMax: 8\n"
                              "      CoefficientTruncationLimit: 0.\n"
                              "      InitialValues: Spherical\n"
-                             "      SizeInitialValues: [1.2, 0.0, 0.0]\n")
+                             "      SizeInitialValues: [1.2, 0.0, 0.0]\n"
+                             "      TransitionEndsAtCube: false\n")
                           : "  TimeDependentMaps: None\n"};
 
   const std::string boundary_conditions{
